3f7c200d4728b110fbc59d9332b021b21c0eb95b
nikitazherebtsov <nikita@crimsongold.ru>
2025-10-11 00:45:10 +0500

    test: :white_check_mark: enable tests

--- lib/metadata/typeDescription/exportTypeDescriptionToXML.test.ts
+++ lib/metadata/typeDescription/exportTypeDescriptionToXML.test.ts
@@ -3,68 +3,68 @@ import xmlExport from "~/lib/xml/export/exporter"
 import { TTypeDescription } from "./types"
 import exportTypeDescriptionToXML from "./exportTypeDescriptionToXML"
 
-// it("should export string type to XML", () => {
-//   const mockTypeDescription: TTypeDescription = {
-//     type: ["string"],
-//     stringQualifiers: {
-//       length: 10,
-//       allowedLength: "Variable",
-//     },
-//   }
+it("should export string type to XML", () => {
+  const mockTypeDescription: TTypeDescription = {
+    type: ["string"],
+    stringQualifiers: {
+      length: 10,
+      allowedLength: "Variable",
+    },
+  }
 
-//   const expectedXml = `<v8:Type>xs:string</v8:Type>
-// <v8:StringQualifiers>
-// 	<v8:Length>10</v8:Length>
-// 	<v8:AllowedLength>Variable</v8:AllowedLength>
-// </v8:StringQualifiers>`
+  const expectedXml = `<v8:Type>xs:string</v8:Type>
+<v8:StringQualifiers>
+	<v8:Length>10</v8:Length>
+	<v8:AllowedLength>Variable</v8:AllowedLength>
+</v8:StringQualifiers>`
 
-//   const result = exportTypeDescriptionToXML(mockTypeDescription)
-//   const xmlString = xmlExport(result, false)
+  const result = exportTypeDescriptionToXML(mockTypeDescription)
+  const xmlString = xmlExport(result, false)
 
-//   expect(xmlString).toEqual(expectedXml)
-// })
+  expect(xmlString).toEqual(expectedXml)
+})
 
-// it("should export number type to XML", () => {
-//   const mockTypeDescription: TTypeDescription = {
-//     type: ["decimal"],
-//     numberQualifiers: {
-//       digits: 10,
-//       fractionDigits: 2,
-//       allowedSign: "Nonnegative",
-//     },
-//   }
+it("should export number type to XML", () => {
+  const mockTypeDescription: TTypeDescription = {
+    type: ["decimal"],
+    numberQualifiers: {
+      digits: 10,
+      fractionDigits: 2,
+      allowedSign: "Nonnegative",
+    },
+  }
 
-//   const expectedXml = `<v8:Type>xs:decimal</v8:Type>
-// <v8:NumberQualifiers>
-// 	<v8:Digits>10</v8:Digits>
-// 	<v8:FractionDigits>2</v8:FractionDigits>
-// 	<v8:AllowedSign>Nonnegative</v8:AllowedSign>
-// </v8:NumberQualifiers>`
+  const expectedXml = `<v8:Type>xs:decimal</v8:Type>
+<v8:NumberQualifiers>
+	<v8:Digits>10</v8:Digits>
+	<v8:FractionDigits>2</v8:FractionDigits>
+	<v8:AllowedSign>Nonnegative</v8:AllowedSign>
+</v8:NumberQualifiers>`
 
-//   const result = exportTypeDescriptionToXML(mockTypeDescription)
-//   const xmlString = xmlExport(result, false)
+  const result = exportTypeDescriptionToXML(mockTypeDescription)
+  const xmlString = xmlExport(result, false)
 
-//   expect(xmlString).toEqual(expectedXml)
-// })
+  expect(xmlString).toEqual(expectedXml)
+})
 
-// it("should export date type to XML", () => {
-//   const mockTypeDescription: TTypeDescription = {
-//     type: ["date"],
-//     dateQualifiers: {
-//       dateFractions: "Date",
-//     },
-//   }
+it("should export date type to XML", () => {
+  const mockTypeDescription: TTypeDescription = {
+    type: ["date"],
+    dateQualifiers: {
+      dateFractions: "Date",
+    },
+  }
 
-//   const expectedXml = `<v8:Type>xs:date</v8:Type>
-// <v8:DateQualifiers>
-// 	<v8:DateFractions>Date</v8:DateFractions>
-// </v8:DateQualifiers>`
+  const expectedXml = `<v8:Type>xs:date</v8:Type>
+<v8:DateQualifiers>
+	<v8:DateFractions>Date</v8:DateFractions>
+</v8:DateQualifiers>`
 
-//   const result = exportTypeDescriptionToXML(mockTypeDescription)
-//   const xmlString = xmlExport(result, false)
+  const result = exportTypeDescriptionToXML(mockTypeDescription)
+  const xmlString = xmlExport(result, false)
 
-//   expect(xmlString).toEqual(expectedXml)
-// })
+  expect(xmlString).toEqual(expectedXml)
+})
 
 it("should export complex type to XML", () => {
   const mockTypeDescription: TTypeDescription = {
