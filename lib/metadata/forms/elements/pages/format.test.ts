fe10e10dcb8e565d639146a1352c198f1050076c
nikita <nikita@crimsongold.ru>
2025-11-15 18:38:20 +0500

    refactor: update formatPages test to enhance formatting logic for page elements, including adjustments to mock data structure and expected results for improved clarity and consistency

--- lib/metadata/forms/elements/pages/format.test.ts
+++ lib/metadata/forms/elements/pages/format.test.ts
@@ -1,49 +1,45 @@
 import { expect, it, describe } from "vitest"
 import "~/lib/metadata/forms/elements/inputField/registration"
+import "~/lib/metadata/forms/elements/usualGroup/registration"
 import { ZElementType } from "../types"
 import { TInputField } from "../inputField/types"
 import { formatUsualGroup } from "../usualGroup/format"
 import { TUsualGroup } from "../usualGroup/types"
+import { TPages } from "./types"
+import { formatPages } from "./format"
 
-describe("formatOneLineGroup", () => {
-  it("should format one-line group", () => {
-    const mockElement: TUsualGroup = {
-      name: "Группа",
+describe("formatPages", () => {
+  it("should format pages", () => {
+    const mockElement = {
+      name: "Страницы",
       id: "1",
-      elementType: ZElementType.enum.UsualGroup,
+      elementType: ZElementType.enum.Pages,
       childItems: [
-        { name: "Элемент1", id: "1", elementType: ZElementType.enum.InputField } as TInputField,
-        { name: "Элемент2", id: "2", elementType: ZElementType.enum.InputField } as TInputField,
+        {
+          name: "Группа",
+          id: "1",
+          elementType: ZElementType.enum.UsualGroup,
+          childItems: [
+            {
+              name: "Элемент1",
+              id: "1",
+              elementType: ZElementType.enum.InputField,
+            } as TInputField,
+            {
+              name: "Элемент2",
+              id: "2",
+              elementType: ZElementType.enum.InputField,
+            } as TInputField,
+          ],
+        } as TUsualGroup,
       ],
-    }
+    } as TPages
 
-    const expectedResult = `{Группа}
-Элемент1: {Элемент1} & Элемент2: {Элемент2}`
+    const expectedResult = `//{Страницы}
+  ?UsualGroup {Группа}`
 
-    const result = formatUsualGroup(mockElement, {})
+    const result = formatPages(mockElement, {})
 
     expect(result.strings.join("\n")).toEqual(expectedResult)
   })
-})
-
-it("should format vertical group", () => {
-  const mockElement: TUsualGroup = {
-    name: "Группа",
-    group: "Vertical",
-    title: { items: { ru: "Заголовок группы" } },
-    elementType: ZElementType.enum.UsualGroup,
-    id: "1",
-    childItems: [
-      { name: "Элемент1", id: "1", elementType: ZElementType.enum.InputField } as TInputField,
-      { name: "Элемент2", id: "2", elementType: ZElementType.enum.InputField } as TInputField,
-    ],
-  }
-
-  const expectedResult = `#Заголовок группы {Группа}
-Элемент1: {Элемент1}
-Элемент2: {Элемент2}`
-
-  const result = formatUsualGroup(mockElement, {})
-
-  expect(result.strings.join("\n")).toEqual(expectedResult)
 })
