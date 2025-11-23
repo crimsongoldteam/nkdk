65cf77aa904f9174cc8dbee6f190eac31c7e708b
nikita <nikita@crimsongold.ru>
2025-11-23 17:18:25 +0500

    refactor: enhance parseTree tests to cover various group parsing scenarios, including horizontal and vertical groups, nested groups, and simplified group structures

--- lib/parser/elementsParser/parseTree.test.ts
+++ lib/parser/elementsParser/parseTree.test.ts
@@ -1,14 +1,16 @@
 import { describe, expect, it } from "vitest"
 import { ZElementType } from "~/lib/metadata/forms/elements/types"
 import { TUsualGroup } from "~/lib/metadata/forms/elements/usualGroup/types"
+import * as SE from "~/lib/metadata/systemEnumerations/types"
 import { parseTree } from "./parseTree"
 
 describe("parseTree", () => {
-  it("should one-line group", () => {
-    const mock = `%Однострочная группа {ОднострочнаяГруппа}% {Элемент1}; {Элемент2}`
+  it("should parse one-line group", () => {
+    const mock = `%{ОднострочнаяГруппа} {Элемент1}; {Элемент2}`
 
     const expectedResult: TUsualGroup = {
       elementType: ZElementType.enum.UsualGroup,
+      group: SE.ZColumnsGroup.enum.Horizontal,
       name: "ОднострочнаяГруппа",
       title: { items: { ru: "Однострочная группа" } },
       childItems: [
@@ -22,6 +24,108 @@ describe("parseTree", () => {
     const result = parseTree(mock)
 
     expect(result).toEqual(expectedResult)
+  })
+
+  it("should parse horizontal group with content", () => {
+    const mock = `%{Группа}
+\t{Элемент1}
+\t{Элемент2}`
+    const expectedResult: TUsualGroup = {
+      elementType: ZElementType.enum.UsualGroup,
+      group: SE.ZColumnsGroup.enum.Horizontal,
+      name: "Группа",
+      childItems: [
+        {
+          elementType: ZElementType.enum.GroupContent,
+          content: "Элемент1\nЭлемент2",
+        },
+      ],
+    }
+
+    const result = parseTree(mock)
+
+    expect(result).toEqual(expectedResult)
+  })
+
+  it("should parse vertical group with content", () => {
+    const mock = `#{Группа}
+\t{Элемент1}
+\t{Элемент2}`
+    const expectedResult: TUsualGroup = {
+      elementType: ZElementType.enum.UsualGroup,
+      group: SE.ZColumnsGroup.enum.Vertical,
+      name: "Группа",
+      childItems: [
+        {
+          elementType: ZElementType.enum.GroupContent,
+          content: "Элемент1\nЭлемент2",
+        },
+      ],
+    }
+  })
+
+  it("should parse simplifyed horizontal group with content and properties", () => {
+    const mock = `%{ГоризонтальнаяГруппа}
+\t#{ВертикальнаяГруппа1} #{ВертикальнаяГруппа2}
+\t{Элемент1} + {Элемент2}`
+    const expectedResult: TUsualGroup = {
+      elementType: ZElementType.enum.UsualGroup,
+      group: SE.ZColumnsGroup.enum.Horizontal,
+      name: "ГоризонтальнаяГруппа",
+      childItems: [
+        {
+          elementType: ZElementType.enum.UsualGroup,
+          name: "ВертикальнаяГруппа1",
+          group: SE.ZColumnsGroup.enum.Vertical,
+          childItems: [
+            {
+              elementType: ZElementType.enum.GroupContent,
+              content: "Элемент1",
+            },
+          ],
+        },
+        {
+          elementType: ZElementType.enum.UsualGroup,
+          name: "ВертикальнаяГруппа2",
+          group: SE.ZColumnsGroup.enum.Vertical,
+          childItems: [
+            {
+              elementType: ZElementType.enum.GroupContent,
+              content: "Элемент2",
+            },
+      ],
+
+
+    const result = parseTree(mock)
+
+    expect(result).toEqual(expectedResult)
+  })  
+
+  it("should parse nested groups", () => {
+    const mock = `%{Группа1}
+\t#{Группа2}
+\t\t{Элемент1}`
+
+
+  const expectedResult: TUsualGroup = {
+    elementType: ZElementType.enum.UsualGroup,
+    name: "Группа1",
+    childItems: [
+      {
+        elementType: ZElementType.enum.UsualGroup,
+        name: "Группа2",
+        childItems: [
+          {
+            elementType: ZElementType.enum.GroupContent,
+            content: "Элемент1",
+          },
+        ],
+      },
+    ],
+ 
+  const result = parseTree(mock)
+
+  expect(result).toEqual(expectedResult)
   })
 })
 
