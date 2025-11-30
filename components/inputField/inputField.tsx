71a70cd44761f394e82e5211fd2b3a48a91b289d
nikita <nikita@crimsongold.ru>
2025-11-30 16:02:40 +0500

    refactor: simplify state management in InputField component by replacing useState hooks with direct prop assignments for improved readability

--- components/inputField/inputField.tsx
+++ components/inputField/inputField.tsx
@@ -7,17 +7,16 @@ import {
 } from "@ant-design/icons"
 import { Button, Input, Space, Typography } from "antd"
 import type React from "react"
-import { useState } from "react"
 import type { TInputField } from "~/lib/metadata/forms/elements/inputField/types"
 
 export function InputField(props: Readonly<TInputField>): React.ReactNode {
-  const [title] = useState(props.title?.items.ru || "")
-  const [name] = useState(props.name)
-  const [dropListButton] = useState(props.dropListButton)
-  const [clearButton] = useState(props.clearButton)
-  const [openButton] = useState(props.openButton)
-  const [spinButton] = useState(props.spinButton)
-  const [choiceButton] = useState(props.choiceButton)
+  const title = props.title?.items.ru || ""
+  const name = props.name
+  const dropListButton = props.dropListButton
+  const clearButton = props.clearButton
+  const openButton = props.openButton
+  const spinButton = props.spinButton
+  const choiceButton = props.choiceButton
 
   return (
     <>
