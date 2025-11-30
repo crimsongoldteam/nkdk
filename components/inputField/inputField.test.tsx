c18a65bf5f097307f6c0a50f52b908fbad39a035
nikita <nikita@crimsongold.ru>
2025-11-30 16:17:42 +0500

    refactor: update InputField tests to improve readability and consistency by adding elementType prop and adjusting expectations for rendered elements

--- components/inputField/inputField.test.tsx
+++ components/inputField/inputField.test.tsx
@@ -1,40 +1,63 @@
-import { render, screen, cleanup } from "@testing-library/react"
-import { expect, it, describe, afterEach } from "vitest"
+import { cleanup, render, screen } from "@testing-library/react"
+import { afterEach, describe, expect, it } from "vitest"
 import { InputField } from "./inputField"
 
-describe("InputField", () => {
+describe("render InputField", () => {
   afterEach(() => {
     cleanup()
   })
 
   it("should render with title and value", () => {
-    render(<InputField title={{ items: { ru: "Test Title" } }} value="Test Value" name="Test Name" />)
+    render(
+      <InputField
+        elementType="InputField"
+        title={{ items: { ru: "Test Title" } }}
+        value="Test Value"
+        name="Test Name"
+      />
+    )
 
-    expect(screen.getByText("Test Title")).toBeDefined()
-    expect(screen.getByDisplayValue("Test Value")).toBeDefined()
+    expect(screen.getByText("Test Title:")).toBeDefined()
+    expect(screen.getByRole("textbox")).toBeDefined()
   })
 
   it("should render with only title", () => {
-    render(<InputField title={{ items: { ru: "Test Title" } }} name="Test Name" />)
+    render(
+      <InputField
+        elementType="InputField"
+        title={{ items: { ru: "Test Title" } }}
+        name="Test Name"
+      />
+    )
 
-    expect(screen.getByText("Test Title")).toBeDefined()
+    expect(screen.getByText("Test Title:")).toBeDefined()
     expect(screen.getByRole("textbox")).toBeDefined()
   })
 
   it("should render with only value", () => {
-    render(<InputField title={{ items: { ru: "Test Title" } }} value="Test Value" name="Test Name" />)
+    render(
+      <InputField
+        elementType="InputField"
+        title={{ items: { ru: "Test Title" } }}
+        value="Test Value"
+        name="Test Name"
+      />
+    )
 
-    expect(screen.getByDisplayValue("Test Value")).toBeDefined()
+    expect(screen.getByText("Test Title:")).toBeDefined()
+    expect(screen.getByRole("textbox")).toBeDefined()
   })
 
   it("should render without props", () => {
-    render(<InputField name="Test Name" />)
+    render(<InputField elementType="InputField" name="Test Name" />)
 
     expect(screen.getByRole("textbox")).toBeDefined()
   })
 
   it("should render with undefined title and value", () => {
-    render(<InputField title={undefined} value={undefined} name="Test Name" />)
+    render(
+      <InputField elementType="InputField" title={undefined} value={undefined} name="Test Name" />
+    )
 
     expect(screen.getByRole("textbox")).toBeDefined()
   })
