f95ded327e736f04ddcac83bc1c793abbcd8fd78
nikita <nikita@crimsongold.ru>
2025-12-15 14:18:26 +0500

    chore: add task to format all metadata files and create formatting script; enhance code consistency and maintainability

---
+++ tasks/format-metadata.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -euo pipefail
+
+root="$(cd "$(dirname "$0")/.." && pwd)"
+
+find "$root/lib/metadata/" \( -name "types.ts" -o -name "importFromXML.ts" -o -name "exportToXML.ts" \) -type f -exec npx organize-imports-cli {} \;
+
+find "$root/lib/metadata/" \( -name "types.ts" -o -name "importFromXML.ts" -o -name "exportToXML.ts" \) -type f -exec npx prettier --write {} \;
+
