be579decbf082275e27a7224119e4776c43a11c4
nikita <nikita@crimsongold.ru>
2025-12-20 15:29:31 +0500

    test: enhance importMetadataTabularSectionFromXML tests to cover scenarios with one and two attributes, ensuring accurate XML import handling and expected results

--- lib/metadata/commonObjects/metadataTabularSection/importFromXML.test.ts
+++ lib/metadata/commonObjects/metadataTabularSection/importFromXML.test.ts
@@ -4,22 +4,60 @@ import { describe, expect, it } from "vitest"
 import { mockConfigurationSettings } from "~/lib/tests/mockConfigurationSettings"
 import xmlImport from "~/lib/xml/import/importer"
 import { importMetadataTabularSectionFromXML } from "./importFromXML"
-import { MetadataTabularSectionXML } from "./types"
+import { MetadataTabularSection, MetadataTabularSectionXML } from "./types"
 
 describe("importMetadataTabularSectionFromXML", () => {
-  it("should import metadata tabular section from XML", () => {
+  it("should import with one attribute", () => {
     const xml = readFileSync(join(process.cwd(), "tests/fixtures/metadataTabularSection/oneAttribute.xml"), "utf-8")
-    const expectedResult = {
+
+    const expectedResult: MetadataTabularSection = {
       name: "Контакты",
-      comment: undefined,
-      fillChecking: "DontCheck",
-      lineNumberLength: undefined,
-      objectBelonging: undefined,
-      standardAttributes: undefined,
       synonym: { items: { ru: "Контакты" } },
-      tooltip: undefined,
-      use: undefined,
+      attributes: [
+        {
+          name: "Наименование",
+          synonym: { items: { ru: "Имя Фамилия" } },
+          type: {
+            type: ["string"],
+            stringQualifiers: { allowedLength: "Variable", length: 0 },
+          },
+          fullTextSearch: "DontUse",
+        },
+      ],
     }
+    const xmlData = xmlImport<{ TabularSection: MetadataTabularSectionXML }>(xml)
+    const result = importMetadataTabularSectionFromXML(xmlData.TabularSection, mockConfigurationSettings)
+    expect(result).toEqual(expectedResult)
+  })
+
+  it("should import with two attributes", () => {
+    const xml = readFileSync(join(process.cwd(), "tests/fixtures/metadataTabularSection/twoAttributes.xml"), "utf-8")
+
+    const expectedResult: MetadataTabularSection = {
+      name: "Контакты",
+      synonym: { items: { ru: "Контакты" } },
+      attributes: [
+        {
+          name: "Наименование",
+          synonym: { items: { ru: "Имя Фамилия" } },
+          type: {
+            type: ["string"],
+            stringQualifiers: { allowedLength: "Variable", length: 0 },
+          },
+          fullTextSearch: "DontUse",
+        },
+        {
+          name: "ИдентификаторСтрокиТабличнойЧасти",
+          synonym: { items: { ru: "Идентификатор строки табличной части" } },
+          type: {
+            type: ["decimal"],
+            numberQualifiers: { digits: 7, fractionDigits: 0, allowedSign: "Any" },
+          },
+          fullTextSearch: "DontUse",
+        },
+      ],
+    }
+
     const xmlData = xmlImport<{ TabularSection: MetadataTabularSectionXML }>(xml)
     const result = importMetadataTabularSectionFromXML(xmlData.TabularSection, mockConfigurationSettings)
     expect(result).toEqual(expectedResult)
