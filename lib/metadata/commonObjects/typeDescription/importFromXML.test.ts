5de1833f717e4d7f168ef5e4eb03c1a0f3534a32
nikita <nikita@crimsongold.ru>
2025-12-21 20:35:06 +0500

    fix: refactor import tests for type descriptions to utilize readAndParseXMLFile; remove deprecated XML mock data and improve maintainability by using external XML files

--- lib/metadata/commonObjects/typeDescription/importFromXML.test.ts
+++ lib/metadata/commonObjects/typeDescription/importFromXML.test.ts
@@ -1,28 +1,18 @@
-import { readFileSync } from "fs"
-import { join } from "path"
 import { assertEquals } from "typia"
 import { describe, expect, it } from "vitest"
 import { mockConfigurationSettings } from "~/lib/tests/mockConfigurationSettings"
-import { xmlImport } from "~/lib/xml/import/importer"
+import { readAndParseXMLFile } from "~/lib/tests/readAndParseXMLFile"
 import { importTypeDescriptionFromXML } from "./importFromXML"
 import { TypeDescription, TypeDescriptionXML } from "./types"
 
 describe("importTypeDescriptionFromXML", () => {
   it("should import string type from XML", () => {
-    const mockXml = `<TypeDescription>
-    <v8:Type>xs:string</v8:Type>
-    <v8:StringQualifiers>
-        <v8:Length>10</v8:Length>
-        <v8:AllowedLength>Variable</v8:AllowedLength>
-    </v8:StringQualifiers>
-    </TypeDescription>`
+    const xmlData = readAndParseXMLFile<{ TypeDescription?: TypeDescriptionXML }>("typeDescription/stringType.xml")
 
     const mockResult: TypeDescription = {
       type: ["string"],
       stringQualifiers: { length: 10, allowedLength: "Variable" },
     }
-
-    const xmlData = xmlImport<{ TypeDescription?: TypeDescriptionXML }>(mockXml)
 
     expect(assertEquals<TypeDescriptionXML>(xmlData.TypeDescription)).toEqual(xmlData.TypeDescription)
 
@@ -32,14 +22,7 @@ describe("importTypeDescriptionFromXML", () => {
   })
 
   it("should import number type from XML", () => {
-    const mockXml = `<TypeDescription>
-  <v8:Type>xs:decimal</v8:Type>
-  <v8:NumberQualifiers>
-      <v8:Digits>10</v8:Digits>
-      <v8:FractionDigits>2</v8:FractionDigits>
-      <v8:AllowedSign>Nonnegative</v8:AllowedSign>
-  </v8:NumberQualifiers> 
-</TypeDescription>`
+    const xmlData = readAndParseXMLFile<{ TypeDescription?: TypeDescriptionXML }>("typeDescription/numberType.xml")
 
     const mockResult: TypeDescription = {
       type: ["decimal"],
@@ -50,8 +33,6 @@ describe("importTypeDescriptionFromXML", () => {
       },
     }
 
-    const xmlData = xmlImport<{ TypeDescription?: TypeDescriptionXML }>(mockXml)
-
     expect(assertEquals<TypeDescriptionXML>(xmlData.TypeDescription)).toEqual(xmlData.TypeDescription)
 
     const result = importTypeDescriptionFromXML(xmlData.TypeDescription, mockConfigurationSettings)
@@ -60,19 +41,12 @@ describe("importTypeDescriptionFromXML", () => {
   })
 
   it("should import date type from XML", () => {
-    const mockXml = `<TypeDescription>
-    <v8:Type>xs:dateTime</v8:Type>
-      <v8:DateQualifiers>
-          <v8:DateFractions>Date</v8:DateFractions>
-      </v8:DateQualifiers>
-      </TypeDescription>`
+    const xmlData = readAndParseXMLFile<{ TypeDescription?: TypeDescriptionXML }>("typeDescription/dateTimeType.xml")
 
     const mockResult: TypeDescription = {
       type: ["dateTime"],
       dateQualifiers: { dateFractions: "Date" },
     }
-
-    const xmlData = xmlImport<{ TypeDescription?: TypeDescriptionXML }>(mockXml)
 
     expect(assertEquals<TypeDescriptionXML>(xmlData.TypeDescription)).toEqual(xmlData.TypeDescription)
 
@@ -82,16 +56,11 @@ describe("importTypeDescriptionFromXML", () => {
   })
 
   it("should import complex type from XML", () => {
-    const mockXml = `<TypeDescription>
-    <v8:Type>xs:boolean</v8:Type>
-    <v8:Type>cfg:EnumRef.Статусы</v8:Type>
-    </TypeDescription>`
+    const xmlData = readAndParseXMLFile<{ TypeDescription?: TypeDescriptionXML }>("typeDescription/complexType.xml")
 
     const mockResult: TypeDescription = {
       type: ["boolean", "EnumRef.Статусы"],
     }
-
-    const xmlData = xmlImport<{ TypeDescription?: TypeDescriptionXML }>(mockXml)
 
     expect(assertEquals<TypeDescriptionXML>(xmlData.TypeDescription)).toEqual(xmlData.TypeDescription)
 
@@ -101,17 +70,11 @@ describe("importTypeDescriptionFromXML", () => {
   })
 
   it("should import three types from XML", () => {
-    const mockXml = `<TypeDescription>
-    <v8:Type>cfg:CatalogRef.Сотрудники</v8:Type>
-    <v8:Type>cfg:CatalogRef.Контрагенты</v8:Type>
-    <v8:Type>cfg:CatalogRef.Пользователи</v8:Type>
-    </TypeDescription>`
+    const xmlData = readAndParseXMLFile<{ TypeDescription?: TypeDescriptionXML }>("typeDescription/threeTypes.xml")
 
     const mockResult: TypeDescription = {
       type: ["CatalogRef.Сотрудники", "CatalogRef.Контрагенты", "CatalogRef.Пользователи"],
     }
-
-    const xmlData = xmlImport<{ TypeDescription?: TypeDescriptionXML }>(mockXml)
 
     expect(assertEquals<TypeDescriptionXML>(xmlData.TypeDescription)).toEqual(xmlData.TypeDescription)
 
@@ -121,15 +84,13 @@ describe("importTypeDescriptionFromXML", () => {
   })
 
   it("should import SpreadsheetDocument from XML", () => {
-    const mockXml = `<TypeDescription>
-    <v8:Type xmlns:mxl="http://v8.1c.ru/8.2/data/spreadsheet">mxl:SpreadsheetDocument</v8:Type>
-    </TypeDescription>`
+    const xmlData = readAndParseXMLFile<{ TypeDescription?: TypeDescriptionXML }>(
+      "typeDescription/spreadsheetDocumentType.xml"
+    )
 
     const mockResult: TypeDescription = {
       type: ["SpreadsheetDocument"],
     }
-
-    const xmlData = xmlImport<{ TypeDescription?: TypeDescriptionXML }>(mockXml)
 
     expect(assertEquals<TypeDescriptionXML>(xmlData.TypeDescription)).toEqual(xmlData.TypeDescription)
 
@@ -139,13 +100,11 @@ describe("importTypeDescriptionFromXML", () => {
   })
 
   it("should import type set from XML", () => {
-    const xml = readFileSync(join(process.cwd(), "tests/fixtures/typeDescription/typeSet.xml"), "utf-8")
+    const xmlData = readAndParseXMLFile<{ TypeDescription?: TypeDescriptionXML }>("typeDescription/typeSet.xml")
 
     const mockResult: TypeDescription = {
       type: ["Characteristic.ДополнительныеРеквизитыИСведения"],
     }
-
-    const xmlData = xmlImport<{ TypeDescription?: TypeDescriptionXML }>(xml)
 
     expect(assertEquals<TypeDescriptionXML>(xmlData.TypeDescription)).toEqual(xmlData.TypeDescription)
 
@@ -155,10 +114,7 @@ describe("importTypeDescriptionFromXML", () => {
   })
 
   it("should import empty type from XML", () => {
-    const mockXml = `<TypeDescription>
-  </TypeDescription>`
-
-    const xmlData = xmlImport<{ TypeDescription?: TypeDescriptionXML }>(mockXml)
+    const xmlData = readAndParseXMLFile<{ TypeDescription?: TypeDescriptionXML }>("typeDescription/emptyType.xml")
 
     expect(assertEquals<TypeDescriptionXML>(xmlData.TypeDescription)).toEqual(xmlData.TypeDescription)
 
