60644322013fc1796d0ccacb2776d35e8f8fe199
nikita <nikita@crimsongold.ru>
2025-12-21 20:06:05 +0500

    fix: refactor import test for metadata commands to utilize readAndParseXMLFile; enhance clarity and maintainability by removing deprecated XML reading methods

--- lib/metadata/appliedObjects/metadataCommand/importFromXML.test.ts
+++ lib/metadata/appliedObjects/metadataCommand/importFromXML.test.ts
@@ -1,20 +1,16 @@
-import { readFileSync } from "fs"
-import { join } from "path"
 import { assertEquals } from "typia"
 import { describe, expect, it } from "vitest"
 import { simpleCommand } from "~/lib/tests/fixtures/metadataCommand/simple"
 import { mockConfigurationSettings } from "~/lib/tests/mockConfigurationSettings"
-import xmlImport from "~/lib/xml/import/importer"
+import { readAndParseXMLFile } from "~/lib/tests/readAndParseXMLFile"
 import { importMetadataCommandFromXML } from "./importFromXML"
 import { MetadataCommandXML } from "./types"
 
 describe("importMetadataCommandFromXML", () => {
   it("should import metadata command from XML", () => {
-    const xml = readFileSync(join(process.cwd(), "tests/fixtures/metadataCommand/simple.xml"), "utf-8")
+    const xmlData = readAndParseXMLFile<{ Command: MetadataCommandXML }>("metadataCommand/simple.xml")
 
     const expectedResult = simpleCommand
-
-    const xmlData = xmlImport<{ Command: MetadataCommandXML }>(xml)
 
     expect(assertEquals<MetadataCommandXML>(xmlData.Command)).toEqual(xmlData.Command)
 
