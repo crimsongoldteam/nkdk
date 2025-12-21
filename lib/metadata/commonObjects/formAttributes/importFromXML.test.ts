60a0e39e3a9cc887a1b55b176e980570fb68e5ee
nikita <nikita@crimsongold.ru>
2025-12-21 20:17:25 +0500

    fix: refactor importFromXML functions and tests to improve XML handling and type safety; update test cases to utilize readAndParseXMLFile for better maintainability

--- lib/metadata/commonObjects/formAttributes/importFromXML.test.ts
+++ lib/metadata/commonObjects/formAttributes/importFromXML.test.ts
@@ -1,26 +1,12 @@
 import { describe, expect, it } from "vitest"
-import { xmlImport } from "~/lib"
 import { mockConfigurationSettings } from "~/lib/tests/mockConfigurationSettings"
-import importAttributeFromXML from "./importFromXML"
-import { FormAttribute, FormAttributeItemXML } from "./types"
+import { readAndParseXMLFile } from "~/lib/tests/readAndParseXMLFile"
+import { importFormAttributeFromXML } from "./importFromXML"
+import { FormAttribute, FormAttributeXML, FormAttributesXML } from "./types"
 
-describe("importAttributeFromXML", () => {
+describe("importFormAttributeFromXML", () => {
   it("should import attribute from XML", () => {
-    const mockXml = `<Attribute name="Поле" id="1">
-			<Title>
-				<v8:item>
-					<v8:lang>ru</v8:lang>
-					<v8:content>Заголовок поля</v8:content>
-				</v8:item>
-			</Title>
-			<Type>
-				<v8:Type>xs:string</v8:Type>
-				<v8:StringQualifiers>
-					<v8:Length>0</v8:Length>
-					<v8:AllowedLength>Variable</v8:AllowedLength>
-				</v8:StringQualifiers>
-			</Type>
-		</Attribute>`
+    const xmlData = readAndParseXMLFile<FormAttributeXML>("formAttributes/withTitleAndType.xml")
 
     const mockResult: FormAttribute = {
       name: "Поле",
@@ -32,66 +18,44 @@ describe("importAttributeFromXML", () => {
       title: { items: { ru: "Заголовок поля" } },
     }
 
-    const xmlData = xmlImport<{ Attribute: FormAttributeItemXML }>(mockXml)
-
-    const result = importAttributeFromXML(xmlData, mockConfigurationSettings)
+    const result = importFormAttributeFromXML(xmlData, mockConfigurationSettings)
 
     expect(result).toEqual(mockResult)
   })
 
   it("should import attribute with empty type", () => {
-    const mockXml = `<Attribute name="Фамилия" id="1">
- 			<Type/>
-		</Attribute>`
+    const xmlData = readAndParseXMLFile<FormAttributeXML>("formAttributes/withEmptyType.xml")
 
     const mockResult: FormAttribute = {
       name: "Фамилия",
       id: "1",
     }
 
-    const xmlData = xmlImport<AttributeXML>(mockXml)
-
-    const result = importAttributeFromXML(xmlData, mockConfigurationSettings)
+    const result = importFormAttributeFromXML(xmlData, mockConfigurationSettings)
 
     expect(result).toEqual(mockResult)
   })
 
   it("should import stored and main attribute from XML", () => {
-    const mockXml = `<Attribute name="Фамилия" id="1">
- 			<Type>
-				<v8:Type>xs:string</v8:Type>
-			</Type>
-			<MainAttribute>true</MainAttribute>
-      <StoredData>true</StoredData>
-		</Attribute>`
+    const xmlData = readAndParseXMLFile<FormAttributeXML>("formAttributes/withMainAndStored.xml")
 
     const mockResult: FormAttribute = {
       name: "Фамилия",
       id: "1",
-      type: { type: ["string"] },
+      valueType: { type: ["string"] },
       mainAttribute: true,
       storedData: true,
     }
 
-    const xmlData = xmlImport<{ Attributes: AttributeXML[] }>(mockXml)
-
-    const result = importAttributeFromXML(xmlData.Attributes?.[0], mockConfigurationSettings)
+    const result = importFormAttributeFromXML(xmlData, mockConfigurationSettings)
 
     expect(result).toEqual(mockResult)
   })
 
   it("should ignore ConditionalAppearance from XML", () => {
-    const mockXml = `
-  <Attributes>
-    <ConditionalAppearance>
-      <dcsset:item>
-      </dcsset:item>
-    </ConditionalAppearance>
-  </Attributes>`
+    const xmlData = readAndParseXMLFile<{ Attributes: FormAttributesXML }>("formAttributes/conditionalAppearance.xml")
 
-    const xmlData = xmlImport<{ Attributes: AttributeXML[] }>(mockXml)
-
-    const result = importAttributeFromXML(xmlData.Attributes?.[0] as AttributeXML, mockConfigurationSettings)
+    const result = importFormAttributeFromXML(xmlData.Attributes?.[0] as FormAttributeXML, mockConfigurationSettings)
 
     expect(result).toBeUndefined()
   })
--- lib/metadata/commonObjects/formAttributes/importFromXML.ts
+++ lib/metadata/commonObjects/formAttributes/importFromXML.ts
@@ -1,12 +1,12 @@
 import { importI8nTextFromXML } from "~/lib/metadata/commonObjects/i8nText/importFromXML"
 import { importTypeDescriptionFromXML } from "~/lib/metadata/commonObjects/typeDescription/importFromXML"
 import { ConfigurationSettings } from "~/lib/metadata/configurationSettings/types"
-import { AttributeXML, FormAttribute } from "../types"
+import { ConditionalAppearanceXML, FormAttribute, FormAttributes, FormAttributesXML, FormAttributeXML } from "./types"
 
-export default function importAttributeFromXML(
-  xml: AttributeXML,
+export const importFormAttributeFromXML = (
+  xml: FormAttributeXML,
   configurationSettings: ConfigurationSettings
-): FormAttribute | undefined {
+): FormAttribute | undefined => {
   if (!xml.Attribute) {
     return undefined
   }
@@ -14,9 +14,18 @@ export default function importAttributeFromXML(
   return {
     name: xml.Attribute._name,
     id: xml.Attribute._id,
-    type: importTypeDescriptionFromXML(xml.Attribute.Type, configurationSettings),
+    valueType: importTypeDescriptionFromXML(xml.Attribute.Type, configurationSettings),
     mainAttribute: xml.Attribute.MainAttribute,
     storedData: xml.Attribute.StoredData,
     title: importI8nTextFromXML(xml.Attribute.Title, configurationSettings),
   }
+}
+
+export const importFormAttributesFromXML = (
+  xml: FormAttributesXML,
+  configurationSettings: ConfigurationSettings
+): FormAttributes => {
+  return xml.map((item: FormAttributeXML | ConditionalAppearanceXML) =>
+    importFormAttributeFromXML(item, configurationSettings)
+  )
 }
