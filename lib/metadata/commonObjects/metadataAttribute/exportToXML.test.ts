30c2da7b4a6c00a885d3390511b0b100b88a6a52
nikita <nikita@crimsongold.ru>
2025-12-22 20:12:35 +0500

    test: :white_check_mark: fix metadata attribute test

--- lib/metadata/commonObjects/metadataAttribute/exportToXML.test.ts
+++ lib/metadata/commonObjects/metadataAttribute/exportToXML.test.ts
@@ -1,9 +1,8 @@
-import { readFileSync } from "fs"
-import { join } from "path"
 import { describe, expect, it, vi } from "vitest"
 import { multipleAttributes } from "~/lib/tests/fixtures/metadataAttribute/multiple"
 import { singleAttribute } from "~/lib/tests/fixtures/metadataAttribute/single"
 import { mockConfigurationSettings } from "~/lib/tests/mockConfigurationSettings"
+import { readXMLFileAsString } from "~/lib/tests/readAndParseXMLFile"
 import { xmlExport } from "~/lib/xml/export/exporter"
 import { exportMetadataAttributesToXML } from "./exportToXML"
 
@@ -13,21 +12,21 @@ vi.mock("uuid", () => ({
 
 describe("exportMetadataAttributesToXML", () => {
   it("should export single attribute to XML", () => {
-    const expectedResult = readFileSync(join(process.cwd(), "tests/fixtures/metadataAttribute/single.xml"), "utf-8")
+    const expectedResult = readXMLFileAsString("metadataAttribute/single.xml")
 
     const xmlData = exportMetadataAttributesToXML(singleAttribute, mockConfigurationSettings)
 
-    const result = xmlExport({ Attribute: xmlData }, false).trim()
+    const result = xmlExport({ Attribute: xmlData }, false)
 
     expect(result).toEqual(expectedResult)
   })
 
   it("should export multiple attributes to XML", () => {
-    const expectedResult = readFileSync(join(process.cwd(), "tests/fixtures/metadataAttribute/multiple.xml"), "utf-8")
+    const expectedResult = readXMLFileAsString("metadataAttribute/multiple.xml")
 
     const xmlData = exportMetadataAttributesToXML(multipleAttributes, mockConfigurationSettings)
 
-    const result = xmlExport({ Attribute: xmlData }, false).trim()
+    const result = xmlExport({ Attribute: xmlData }, false)
 
     expect(result).toEqual(expectedResult)
   })
