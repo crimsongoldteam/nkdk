0a1decf2b933927223dff5ca5e82b539260220ad
nikita <nikita@crimsongold.ru>
2025-12-24 15:54:29 +0500

    refactor: update ChoiceParameter types to improve structure and consistency; replace dataPath with value and consolidate XML interfaces

--- lib/metadata/commonObjects/сhoiceParameter/types.ts
+++ lib/metadata/commonObjects/сhoiceParameter/types.ts
@@ -1,44 +1,31 @@
-import { I8nTextXML } from "~/lib/metadata/commonObjects/i8nText/types"
-import * as SE from "~/lib/metadata/systemEnumerations/types"
-import { MetadataFieldXML } from "../metadataField/types"
+import { MetadataValue, MetadataValueXML } from "../metadataValue/types"
 
 //#region ChoiceParameter
 
 export interface ChoiceParameter {
   name: string
-  dataPath: string
-  valueChange?: SE.LinkedValueChangeMode
+  value: MetadataValue
+}
+
+export type ChoiceParameters = ChoiceParameter[]
+
+//#endregion
+
+//#region ChoiceParameterXML
+
+export interface ChoiceParameterXML {
+  _name: string
+  "app:value": MetadataValueXML
+}
+
+export interface ChoiceParametersXML {
+  "app:item": ChoiceParameterXML | ChoiceParameterXML[]
 }
 
 //#endregion
 
-//#region ChoiceParameterValueXML
+//#region ChoiceParametersEnterprise
 
-export interface ChoiceParameterSimpleValueXML {
-  "_xsi:type": "xs:string" | "xs:boolean" | "xr:DesignTimeRef"
-  "#text": string
-}
-
-export interface ChoiceParameterFixedArrayValueXML {
-  "_xsi:type": "v8:FixedArray"
-  "v8:Value": MetadataFieldXML | MetadataFieldXML[]
-}
-
-export interface ChoiceParameterFormChoiceListValueXML {
-  "_xsi:type": "FormChoiceListDesTimeValue"
-  Presentation?: I8nTextXML
-  Value: MetadataFieldXML
-}
-
-export type ChoiceParameterLinkValueXML =
-  | ChoiceParameterSimpleValueXML
-  | ChoiceParameterFixedArrayValueXML
-  | ChoiceParameterFormChoiceListValueXML
-
-//#endregion
-
-//#region ChoiceParameterEnterprise
-
-export type ChoiceParameterLinksEnterprise = string
+export type ChoiceParametersEnterprise = string
 
 //#endregion
