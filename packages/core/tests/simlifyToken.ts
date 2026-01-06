a115a019528aaacc1723c1edc9ed5d0131351bf7
nikita <nikita@crimsongold.ru>
2026-01-06 22:26:14 +0500

    refactor: streamline input field validation logic by consolidating checks and improving error handling for enhanced robustness and maintainability

---
+++ packages/core/tests/simlifyToken.ts
@@ -0,0 +1,31 @@
+import { IToken } from "chevrotain"
+import { DetectedTreeNode } from "../parser/detector/detectTree"
+import { ParseElementType } from "../parser/types"
+
+export type TSimplifyToken = { type: string; value: string }
+
+export type SimplifiedDetectedTreeNode = {
+  tokens: TSimplifyToken[]
+  type: ParseElementType
+  childItems: SimplifiedDetectedTreeNode[]
+}
+
+export const simlifyDetectedTreeNodes = (nodes: DetectedTreeNode[]): SimplifiedDetectedTreeNode[] => {
+  return nodes.map((node) => {
+    // Упрощаем токены текущего узла
+    const simplifiedTokens = node.tokens.map((token) => simplifyToken(token))
+
+    // Рекурсивно обрабатываем дочерние элементы
+    const simplifiedChildItems = simlifyDetectedTreeNodes(node.childItems)
+
+    return {
+      tokens: simplifiedTokens,
+      type: node.type,
+      childItems: simplifiedChildItems,
+    }
+  })
+}
+
+const simplifyToken = (token: IToken): TSimplifyToken => {
+  return { type: token.tokenType.name, value: token.image }
+}
