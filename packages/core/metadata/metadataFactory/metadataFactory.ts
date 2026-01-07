2c3a854ad6fd128cbaf83f1bf33d07187f57caa3
nikita <nikita@crimsongold.ru>
2026-01-07 12:38:55 +0500

    refactor: enhance metadata registration and retrieval functions to support ImportFromEnterprise operation, improving type safety and clarity

--- packages/core/metadata/metadataFactory/metadataFactory.ts
+++ packages/core/metadata/metadataFactory/metadataFactory.ts
@@ -3,9 +3,14 @@ import { MetadataType } from "./types"
 
 export type ItemOperationType = "ExportToXML" | "ExportToEnterprise" | "ImportFromXML" | "ImportFromEnterprise"
 
-type OperationFunction<T = any> = (context: ConfigurationContext, data: T, name?: string) => any
+type OperationFunction<T = any> = (context: ConfigurationContext, data: T) => any
+type OperationImportFromEnterpriseFunction<T = any> = (
+  context: ConfigurationContext,
+  data: T | undefined,
+  name: string
+) => any
 
-type OperationRegistry = Map<MetadataType, OperationFunction>
+type OperationRegistry = Map<MetadataType, OperationFunction | OperationImportFromEnterpriseFunction>
 
 const operationRegistries: Map<ItemOperationType, OperationRegistry> = new Map([
   ["ExportToXML", new Map()],
@@ -14,29 +19,43 @@ const operationRegistries: Map<ItemOperationType, OperationRegistry> = new Map([
   ["ImportFromEnterprise", new Map()],
 ])
 
-export const registerMetadata = <T = any>(
-  operationType: ItemOperationType,
+export function registerMetadata<T = any>(
+  operationType: "ImportFromEnterprise",
+  key: MetadataType,
+  operationFunction: OperationImportFromEnterpriseFunction<T>
+): void
+export function registerMetadata<T = any>(
+  operationType: Exclude<ItemOperationType, "ImportFromEnterprise">,
   key: MetadataType,
   operationFunction: OperationFunction<T>
-): void => {
+): void
+export function registerMetadata<T = any>(
+  operationType: ItemOperationType,
+  key: MetadataType,
+  operationFunction: OperationFunction<T> | OperationImportFromEnterpriseFunction<T>
+): void {
   const registry = operationRegistries.get(operationType)
   if (!registry) {
     throw new Error(`Unknown operation type: ${operationType}`)
   }
 
-  registry.set(key, operationFunction as OperationFunction)
+  registry.set(key, operationFunction)
 }
 
-export const getOperationFunction = (
-  operationType: ItemOperationType,
+export const getOperationFunction = <T extends ItemOperationType>(
+  operationType: T,
   key: MetadataType
-): OperationFunction | undefined => {
+): T extends "ImportFromEnterprise"
+  ? OperationImportFromEnterpriseFunction | undefined
+  : OperationFunction | undefined => {
   const registry = operationRegistries.get(operationType)
   if (!registry) {
     throw new Error(`Unknown operation type: ${operationType}`)
   }
 
-  return registry.get(key)
+  return registry.get(key) as T extends "ImportFromEnterprise"
+    ? OperationImportFromEnterpriseFunction | undefined
+    : OperationFunction | undefined
 }
 
 export const executeOperation = <T = any>(
