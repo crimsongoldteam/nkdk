4dad8ab77254243737d9734ea8c4efb1634343ce
nikita <nikita@crimsongold.ru>
2026-01-12 21:39:17 +0500

    refactor: enhance SearchControlAddition types by adding new properties for context menu, tooltips, and button group child items, improving metadata consistency and type handling

--- packages/core/metadata/forms/elements/searchControlAddition/types.ts
+++ packages/core/metadata/forms/elements/searchControlAddition/types.ts
@@ -1,12 +1,21 @@
 import { StringboolEnterprise } from "~/metadata/commonObjects/boolean/types"
 import { Color, ColorEnterprise, ColorXML } from "~/metadata/commonObjects/color/types"
 import { Font, FontEnterprise, FontXML } from "~/metadata/commonObjects/font/types"
+import { I8nText, I8nTextEnterprise, I8nTextXML } from "~/metadata/commonObjects/i8nText/types"
 import { UserVisible, UserVisibleEnterprise, UserVisibleXML } from "~/metadata/commonObjects/userVisible/types"
 import {
   FormItemAddition,
   FormItemAdditionEnterprise,
   FormItemAdditionXML,
 } from "~/metadata/forms/elements/formItemAddition/types"
+import * as SE from "~/metadata/systemEnumerations/types"
+import {
+  ButtonGroupChildItems,
+  ButtonGroupChildItemsEnterprise,
+  ButtonGroupChildItemsXML,
+} from "../../collections/buttonGroupChildItems/types"
+import { ContextMenu, ContextMenuEnterprise, ContextMenuXML } from "../contextMenu/types"
+import { ExtendedTooltip, ExtendedTooltipPropsEnterprise, ExtendedTooltipXML } from "../extendedTooltip/types"
 
 export interface SearchControlAddition extends FormItemAddition {
   autoMaxWidth?: boolean
@@ -16,8 +25,20 @@ export interface SearchControlAddition extends FormItemAddition {
   horizontalStretch?: boolean
   maxWidth?: number
   textColor?: Color
-  userVisible?: UserVisible
   width?: number
+  contextMenu?: ContextMenu
+  displayImportance?: SE.DisplayImportance
+  enabled?: boolean
+  extendedTooltip?: ExtendedTooltip
+  horizontalAlignInGroup?: SE.ItemHorizontalLocation
+  title?: I8nText
+  toolTip?: I8nText
+  toolTipRepresentation?: SE.ToolTipRepresentation
+  type?: SE.FormItemAdditionType
+  userVisible?: UserVisible
+  verticalAlignInGroup?: SE.ItemVerticalAlign
+  visible?: boolean
+  childItems: ButtonGroupChildItems
 }
 
 export interface SearchControlAdditionXML extends FormItemAdditionXML {
@@ -28,19 +49,43 @@ export interface SearchControlAdditionXML extends FormItemAdditionXML {
   HorizontalStretch?: boolean
   MaxWidth?: number
   TextColor?: ColorXML
-  UserVisible?: UserVisibleXML
   Width?: number
+  ChildItems?: ButtonGroupChildItemsXML
+  ContextMenu: ContextMenuXML
+  _DisplayImportance?: SE.DisplayImportance
+  Enabled?: boolean
+  ExtendedTooltip: ExtendedTooltipXML
+  HorizontalAlignInGroup?: SE.ItemHorizontalLocation
+  Title?: I8nTextXML
+  ToolTip?: I8nTextXML
+  ToolTipRepresentation?: SE.ToolTipRepresentation
+  Type?: SE.FormItemAdditionType
+  UserVisible?: UserVisibleXML
+  VerticalAlignInGroup?: SE.ItemVerticalAlign
+  Visible?: boolean
 }
 
 export interface SearchControlAdditionEnterprise extends FormItemAdditionEnterprise {
   АвтоМаксимальнаяШирина?: StringboolEnterprise
   МаксимальнаяШирина?: number
-  РазрешитьИспользование?: UserVisibleEnterprise
-  ЗапретитьИспользование?: UserVisibleEnterprise
   РастягиватьПоГоризонтали?: StringboolEnterprise
   ЦветРамки?: ColorEnterprise
   ЦветТекста?: ColorEnterprise
   ЦветФона?: ColorEnterprise
   Ширина?: number
   Шрифт?: FontEnterprise
+  ВажностьПриОтображении?: SE.DisplayImportanceEnterprise
+  ВертикальноеПоложениеВГруппе?: SE.ItemVerticalAlignEnterprise
+  Вид?: SE.FormItemAdditionTypeEnterprise
+  Видимость?: StringboolEnterprise
+  ГоризонтальноеПоложениеВГруппе?: SE.ItemHorizontalLocationEnterprise
+  Доступность?: StringboolEnterprise
+  Заголовок?: I8nTextEnterprise
+  КонтекстноеМеню?: ContextMenuEnterprise
+  ОтображениеПодсказки?: SE.ToolTipRepresentationEnterprise
+  Подсказка?: I8nTextEnterprise
+  РазрешитьИспользование?: UserVisibleEnterprise
+  ЗапретитьИспользование?: UserVisibleEnterprise
+  РасширеннаяПодсказка?: ExtendedTooltipPropsEnterprise
+  ПодчиненныеЭлементы?: ButtonGroupChildItemsEnterprise
 }
