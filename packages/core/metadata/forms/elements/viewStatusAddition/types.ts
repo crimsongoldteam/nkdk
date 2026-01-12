6122aefa0aaf4193301386fd759c8d5da50394b9
nikita <nikita@crimsongold.ru>
2026-01-12 22:21:18 +0500

    refactor: remove export/import functions and tests for ViewStatusAddition elements, enhancing type structure and improving metadata consistency

--- packages/core/metadata/forms/elements/viewStatusAddition/types.ts
+++ packages/core/metadata/forms/elements/viewStatusAddition/types.ts
@@ -2,15 +2,16 @@ import { StringboolEnterprise } from "~/metadata/commonObjects/boolean/types"
 import { Border, BorderEnterprise, BorderXML } from "~/metadata/commonObjects/border/types"
 import { Color, ColorEnterprise, ColorXML } from "~/metadata/commonObjects/color/types"
 import { Font, FontEnterprise, FontXML } from "~/metadata/commonObjects/font/types"
+import { I8nText, I8nTextEnterprise, I8nTextXML } from "~/metadata/commonObjects/i8nText/types"
 import { UserVisible, UserVisibleEnterprise, UserVisibleXML } from "~/metadata/commonObjects/userVisible/types"
-import {
-  FormItemAddition,
-  FormItemAdditionEnterprise,
-  FormItemAdditionXML,
-} from "~/metadata/forms/elements/formItemAddition/types"
-import * as SE from "~/metadata/systemEnumerations/types"
 
-export interface ViewStatusAddition extends FormItemAddition {
+import * as SE from "~/metadata/systemEnumerations/types"
+import { ChildItemsEnterprise } from "../../collections/childItems/types"
+import { BaseElementXML } from "../baseElement/types"
+import { ContextMenu, ContextMenuEnterprise, ContextMenuXML } from "../contextMenu/types"
+import { ExtendedTooltip, ExtendedTooltipPropsEnterprise, ExtendedTooltipXML } from "../extendedTooltip/types"
+
+export interface ViewStatusAddition {
   autoMaxWidth?: boolean
   backColor?: Color
   border?: Border
@@ -23,11 +24,21 @@ export interface ViewStatusAddition extends FormItemAddition {
   textColor?: Color
   titleFont?: Font
   titleTextColor?: Color
-  userVisible?: UserVisible
   width?: number
+  contextMenu?: ContextMenu
+  displayImportance?: SE.DisplayImportance
+  enabled?: boolean
+  extendedTooltip?: ExtendedTooltip
+  horizontalAlignInGroup?: SE.ItemHorizontalLocation
+  title?: I8nText
+  toolTip?: I8nText
+  toolTipRepresentation?: SE.ToolTipRepresentation
+  userVisible?: UserVisible
+  verticalAlignInGroup?: SE.ItemVerticalAlign
+  visible?: boolean
 }
 
-export interface ViewStatusAdditionXML extends FormItemAdditionXML {
+export interface ViewStatusAdditionXML extends BaseElementXML {
   AutoMaxWidth?: boolean
   BackColor?: ColorXML
   Border?: BorderXML
@@ -42,14 +53,22 @@ export interface ViewStatusAdditionXML extends FormItemAdditionXML {
   TitleTextColor?: ColorXML
   UserVisible?: UserVisibleXML
   Width?: number
+  ContextMenu?: ContextMenuXML
+  _DisplayImportance?: SE.DisplayImportance
+  Enabled?: boolean
+  ExtendedTooltip?: ExtendedTooltipXML
+  HorizontalAlignInGroup?: SE.ItemHorizontalLocation
+  Title?: I8nTextXML
+  ToolTip?: I8nTextXML
+  ToolTipRepresentation?: SE.ToolTipRepresentation
+  VerticalAlignInGroup?: SE.ItemVerticalAlign
+  Visible?: boolean
 }
 
-export interface ViewStatusAdditionEnterprise extends FormItemAdditionEnterprise {
+export interface ViewStatusAdditionEnterprise {
   АвтоМаксимальнаяШирина?: StringboolEnterprise
   ГоризонтальноеПоложение?: SE.ItemHorizontalLocationEnterprise
   МаксимальнаяШирина?: number
-  РазрешитьИспользование?: UserVisibleEnterprise
-  ЗапретитьИспользование?: UserVisibleEnterprise
   Рамка?: BorderEnterprise
   РастягиватьПоГоризонтали?: StringboolEnterprise
   ЦветРамки?: ColorEnterprise
@@ -60,4 +79,18 @@ export interface ViewStatusAdditionEnterprise extends FormItemAdditionEnterprise
   Ширина?: number
   Шрифт?: FontEnterprise
   ШрифтЗаголовка?: FontEnterprise
+  ВажностьПриОтображении?: SE.DisplayImportanceEnterprise
+  ВертикальноеПоложениеВГруппе?: SE.ItemVerticalAlignEnterprise
+  Вид?: SE.FormItemAdditionTypeEnterprise
+  Видимость?: StringboolEnterprise
+  ГоризонтальноеПоложениеВГруппе?: SE.ItemHorizontalLocationEnterprise
+  Доступность?: StringboolEnterprise
+  Заголовок?: I8nTextEnterprise
+  КонтекстноеМеню?: ContextMenuEnterprise
+  ОтображениеПодсказки?: SE.ToolTipRepresentationEnterprise
+  Подсказка?: I8nTextEnterprise
+  ПодчиненныеЭлементы?: ChildItemsEnterprise
+  РазрешитьИспользование?: UserVisibleEnterprise
+  ЗапретитьИспользование?: UserVisibleEnterprise
+  РасширеннаяПодсказка?: ExtendedTooltipPropsEnterprise
 }
