c77e6522aa1ad764a88fd10f3c2d1bff3860d087
nikita <nikita@crimsongold.ru>
2026-01-15 23:10:34 +0500

    refactor: enhance import functions for various field types, adding new properties and improving type safety and metadata consistency

--- packages/core/metadata/forms/elements/formattedDocumentField/importFromEnterprise.ts
+++ packages/core/metadata/forms/elements/formattedDocumentField/importFromEnterprise.ts
@@ -1,6 +1,12 @@
 import { importBooleanFromEnterprise } from "~/metadata/commonObjects/boolean/importFromEnterprise"
 import { importColorFromEnterprise } from "~/metadata/commonObjects/color/importFromEnterprise"
 import { importFontFromEnterprise } from "~/metadata/commonObjects/font/importFromEnterprise"
+import {
+  importI8nTextCombinedFromEnterprise,
+  importI8nTextFromEnterprise,
+} from "~/metadata/commonObjects/i8nText/importFromEnterprise"
+import { importPictureFromEnterprise } from "~/metadata/commonObjects/picture/importFromEnterprise"
+import { importTypeDescriptionFromEnterprise } from "~/metadata/commonObjects/typeDescription/importFromEnterprise"
 import { importUserVisibleFromEnterprise } from "~/metadata/commonObjects/userVisible/importFromEnterprise"
 import { ConfigurationContext } from "~/metadata/context/types"
 import {
@@ -8,12 +14,13 @@ import {
   FormattedDocumentFieldPartialEnterprise,
   FormattedDocumentFieldTypedEnterprise,
 } from "~/metadata/forms/elements/formattedDocumentField/types"
-import { importFormFieldFromEnterprise } from "~/metadata/forms/elements/formField/importFromEnterprise"
 import { importEventsFromEnterprise } from "~/metadata/forms/events/importFromEnterprise"
 import { registerMetadata } from "~/metadata/metadataFactory/metadataFactory"
 import { ToPartialEnterpriseType, ToTypedEnterpriseType } from "~/metadata/metadataFactory/types"
 import { importSystemEnumerationFromEnterprise } from "~/metadata/systemEnumerations/importFromEnterprise"
 import * as SE from "~/metadata/systemEnumerations/types"
+import { importContextMenuFromEnterprise } from "../contextMenu/importFromEnterprise"
+import { importExtendedTooltipFromEnterprise } from "../extendedTooltip/importFromEnterprise"
 
 export function importFormattedDocumentFieldTypedFromEnterprise<To extends FormattedDocumentField | undefined>(
   context: ConfigurationContext,
@@ -22,16 +29,16 @@ export function importFormattedDocumentFieldTypedFromEnterprise<To extends Forma
 ): To {
   if (data === undefined) return undefined as To
 
-  const baseFields = importFormFieldFromEnterprise(context, data, name)!
-
   const props = importFormattedDocumentFieldPropsFromEnterprise(context, data)
 
   const result: FormattedDocumentField = {
-    ...baseFields,
     ...props,
     elementType: "FormattedDocumentField",
     name: name,
   }
+
+  const title = importI8nTextFromEnterprise(context, data?.Заголовок)
+  if (title !== undefined) result.title = title
 
   return result as To
 }
@@ -41,15 +48,15 @@ export function importFormattedDocumentFieldPartialFromEnterprise<To extends For
   source: To,
   data: ToPartialEnterpriseType<To> | undefined
 ): To {
-  const baseFields = importFormFieldFromEnterprise(context, data, source.name)!
-
   const props = importFormattedDocumentFieldPropsFromEnterprise(context, data)
   const result: To = {
     ...source,
-    ...baseFields,
     ...props,
-    elementType: source.elementType, // Сохраняем elementType из source
+    elementType: "FormattedDocumentField",
   }
+
+  const title = importI8nTextCombinedFromEnterprise(context, source.title, data?.Заголовок)
+  if (title !== undefined) result.title = title
 
   return result
 }
@@ -61,6 +68,177 @@ const importFormattedDocumentFieldPropsFromEnterprise = (
   const result: Omit<Partial<FormattedDocumentField>, "elementType" | "name"> = {}
 
   if (data === undefined) return result
+
+  const autoCellHeight = importBooleanFromEnterprise(context, data.АвтоВысотаЯчейки)
+  if (autoCellHeight !== undefined) result.autoCellHeight = autoCellHeight
+
+  const defaultItem = importBooleanFromEnterprise(context, data.АктивизироватьПоУмолчанию)
+  if (defaultItem !== undefined) result.defaultItem = defaultItem
+
+  const displayImportance = importSystemEnumerationFromEnterprise<SE.DisplayImportance>(
+    context,
+    data.ВажностьПриОтображении,
+    SE.DisplayImportanceFromEnterprise
+  )
+  if (displayImportance !== undefined) result.displayImportance = displayImportance
+
+  const verticalAlign = importSystemEnumerationFromEnterprise<SE.ItemVerticalAlign>(
+    context,
+    data.ВертикальноеПоложение,
+    SE.ItemVerticalAlignFromEnterprise
+  )
+  if (verticalAlign !== undefined) result.verticalAlign = verticalAlign
+
+  const verticalAlignInGroup = importSystemEnumerationFromEnterprise<SE.ItemVerticalAlign>(
+    context,
+    data.ВертикальноеПоложениеВГруппе,
+    SE.ItemVerticalAlignFromEnterprise
+  )
+  if (verticalAlignInGroup !== undefined) result.verticalAlignInGroup = verticalAlignInGroup
+
+  const type = importSystemEnumerationFromEnterprise<SE.FormFieldType>(
+    context,
+    data.Вид,
+    SE.FormFieldTypeFromEnterprise
+  )
+  if (type !== undefined) result.type = type
+
+  const visible = importBooleanFromEnterprise(context, data.Видимость)
+  if (visible !== undefined) result.visible = visible
+
+  if (data.ВысотаЗаголовка !== undefined) result.titleHeight = data.ВысотаЗаголовка
+
+  const cellHyperlink = importBooleanFromEnterprise(context, data.ГиперссылкаЯчейки)
+  if (cellHyperlink !== undefined) result.cellHyperlink = cellHyperlink
+
+  const horizontalAlign = importSystemEnumerationFromEnterprise<SE.ItemHorizontalLocation>(
+    context,
+    data.ГоризонтальноеПоложение,
+    SE.ItemHorizontalLocationFromEnterprise
+  )
+  if (horizontalAlign !== undefined) result.horizontalAlign = horizontalAlign
+
+  const horizontalAlignInGroup = importSystemEnumerationFromEnterprise<SE.ItemHorizontalLocation>(
+    context,
+    data.ГоризонтальноеПоложениеВГруппе,
+    SE.ItemHorizontalLocationFromEnterprise
+  )
+  if (horizontalAlignInGroup !== undefined) result.horizontalAlignInGroup = horizontalAlignInGroup
+
+  const footerHorizontalAlign = importSystemEnumerationFromEnterprise<SE.ItemHorizontalLocation>(
+    context,
+    data.ГоризонтальноеПоложениеВПодвале,
+    SE.ItemHorizontalLocationFromEnterprise
+  )
+  if (footerHorizontalAlign !== undefined) result.footerHorizontalAlign = footerHorizontalAlign
+
+  const headerHorizontalAlign = importSystemEnumerationFromEnterprise<SE.ItemHorizontalLocation>(
+    context,
+    data.ГоризонтальноеПоложениеВШапке,
+    SE.ItemHorizontalLocationFromEnterprise
+  )
+  if (headerHorizontalAlign !== undefined) result.headerHorizontalAlign = headerHorizontalAlign
+
+  const enabled = importBooleanFromEnterprise(context, data.Доступность)
+  if (enabled !== undefined) result.enabled = enabled
+
+  const footerPicture = importPictureFromEnterprise(context, data.КартинкаПодвала)
+  if (footerPicture !== undefined) result.footerPicture = footerPicture
+
+  const headerPicture = importPictureFromEnterprise(context, data.КартинкаШапки)
+  if (headerPicture !== undefined) result.headerPicture = headerPicture
+
+  const contextMenu = importContextMenuFromEnterprise(context, data.КонтекстноеМеню)
+  if (contextMenu !== undefined) result.contextMenu = contextMenu
+
+  const typeRestriction = importTypeDescriptionFromEnterprise(context, data.ОграничениеТипа)
+  if (typeRestriction !== undefined) result.typeRestriction = typeRestriction
+
+  const showInFooter = importBooleanFromEnterprise(context, data.ОтображатьВПодвале)
+  if (showInFooter !== undefined) result.showInFooter = showInFooter
+
+  const showInHeader = importBooleanFromEnterprise(context, data.ОтображатьВШапке)
+  if (showInHeader !== undefined) result.showInHeader = showInHeader
+
+  const toolTipRepresentation = importSystemEnumerationFromEnterprise<SE.ToolTipRepresentation>(
+    context,
+    data.ОтображениеПодсказки,
+    SE.ToolTipRepresentationFromEnterprise
+  )
+  if (toolTipRepresentation !== undefined) result.toolTipRepresentation = toolTipRepresentation
+
+  const warningOnEditRepresentation = importSystemEnumerationFromEnterprise<SE.WarningOnEditRepresentation>(
+    context,
+    data.ОтображениеПредупрежденияПриРедактировании,
+    SE.WarningOnEditRepresentationFromEnterprise
+  )
+  if (warningOnEditRepresentation !== undefined) result.warningOnEditRepresentation = warningOnEditRepresentation
+
+  const toolTip = importI8nTextFromEnterprise(context, data.Подсказка)
+  if (toolTip !== undefined) result.toolTip = toolTip
+
+  const titleLocation = importSystemEnumerationFromEnterprise<SE.FormItemTitleLocation>(
+    context,
+    data.ПоложениеЗаголовка,
+    SE.FormItemTitleLocationFromEnterprise
+  )
+  if (titleLocation !== undefined) result.titleLocation = titleLocation
+
+  const warningOnEdit = importI8nTextFromEnterprise(context, data.ПредупреждениеПриРедактировании)
+  if (warningOnEdit !== undefined) result.warningOnEdit = warningOnEdit
+
+  const skipOnInput = importBooleanFromEnterprise(context, data.ПропускатьПриВводе)
+  if (skipOnInput !== undefined) result.skipOnInput = skipOnInput
+
+  if (data.ПутьКДанным !== undefined) result.dataPath = data.ПутьКДанным
+
+  if (data.ПутьКДаннымПодвала !== undefined) result.footerDataPath = data.ПутьКДаннымПодвала
+
+  const extendedTooltip = importExtendedTooltipFromEnterprise(context, data.РасширеннаяПодсказка)
+  if (extendedTooltip !== undefined) result.extendedTooltip = extendedTooltip
+
+  const editMode = importSystemEnumerationFromEnterprise<SE.ColumnEditMode>(
+    context,
+    data.РежимРедактирования,
+    SE.ColumnEditModeFromEnterprise
+  )
+  if (editMode !== undefined) result.editMode = editMode
+
+  if (data.СочетаниеКлавиш !== undefined) result.shortcut = data.СочетаниеКлавиш
+
+  // const table = importTableFromEnterprise(context, data.Таблица, name + ".Таблица")
+  // if (table !== undefined) result.table = table
+
+  const footerText = importI8nTextFromEnterprise(context, data.ТекстПодвала)
+  if (footerText !== undefined) result.footerText = footerText
+
+  const readOnly = importBooleanFromEnterprise(context, data.ТолькоПросмотр)
+  if (readOnly !== undefined) result.readOnly = readOnly
+
+  const fixingInTable = importSystemEnumerationFromEnterprise<SE.FixingInTable>(
+    context,
+    data.ФиксацияВТаблице,
+    SE.FixingInTableFromEnterprise
+  )
+  if (fixingInTable !== undefined) result.fixingInTable = fixingInTable
+
+  const titleTextColor = importColorFromEnterprise(context, data.ЦветТекстаЗаголовка)
+  if (titleTextColor !== undefined) result.titleTextColor = titleTextColor
+
+  const footerTextColor = importColorFromEnterprise(context, data.ЦветТекстаПодвала)
+  if (footerTextColor !== undefined) result.footerTextColor = footerTextColor
+
+  const titleBackColor = importColorFromEnterprise(context, data.ЦветФонаЗаголовка)
+  if (titleBackColor !== undefined) result.titleBackColor = titleBackColor
+
+  const footerBackColor = importColorFromEnterprise(context, data.ЦветФонаПодвала)
+  if (footerBackColor !== undefined) result.footerBackColor = footerBackColor
+
+  const titleFont = importFontFromEnterprise(context, data.ШрифтЗаголовка)
+  if (titleFont !== undefined) result.titleFont = titleFont
+
+  const footerFont = importFontFromEnterprise(context, data.ШрифтПодвала)
+  if (footerFont !== undefined) result.footerFont = footerFont
 
   const autoMaxHeight = importBooleanFromEnterprise(context, data.АвтоМаксимальнаяВысота)
   if (autoMaxHeight !== undefined) result.autoMaxHeight = autoMaxHeight
