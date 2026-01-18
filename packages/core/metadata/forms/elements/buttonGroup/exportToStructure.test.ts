cf846f2e893afaaddc10616ec6800117b433a9e1
nikita <nikita@crimsongold.ru>
2026-01-18 13:29:32 +0500

    refactor: rename export function for button group and update related tests to improve clarity and structure handling

--- packages/core/metadata/forms/elements/buttonGroup/exportToStructure.test.ts
+++ packages/core/metadata/forms/elements/buttonGroup/exportToStructure.test.ts
@@ -1,13 +1,13 @@
 import { describe, expect, it } from "vitest"
 import { buttonGroupStructureFixturesTable } from "~/tests/fixtures/forms/buttonGroup/data"
 import { mockСontext } from "~/tests/mockContext"
-import { exportButtonGroupToStructure } from "./exportToStructure"
+import { exportButtonGroupContentToStructure } from "./exportToStructure"
 
-describe("exportButtonGroupToStructure", () => {
+describe("exportButtonGroupContentToStructure", () => {
   it.each(buttonGroupStructureFixturesTable)(
     "should export button group $name",
     ({ element: input, structured: expected }) => {
-      const result = exportButtonGroupToStructure(mockСontext, input)
+      const result = exportButtonGroupContentToStructure(mockСontext, input)
 
       expect(result.strings).toEqual(expected.strings)
     }
--- packages/core/metadata/forms/elements/buttonGroup/exportToStructure.ts
+++ packages/core/metadata/forms/elements/buttonGroup/exportToStructure.ts
@@ -1,20 +1,9 @@
 import { ConfigurationContext } from "~/metadata/context/types"
 import { IFormatElementResult } from "~/metadata/forms/format/types"
 import { registerMetadata } from "~/metadata/metadataFactory/metadataFactory"
-import { ExportToStructureContentFn, ExportToStructureFn } from "~/metadata/metadataFactory/types"
-import { formatElementTitleAndName, wrapButtonContent } from "../../format/helpers"
+import { ExportToStructureContentFn } from "~/metadata/metadataFactory/types"
+import { formatElementTitleAndName } from "../../format/helpers"
 import { ButtonGroup } from "./types"
-
-export function exportButtonGroupToStructure(
-  context: ConfigurationContext,
-  element: ButtonGroup
-): IFormatElementResult {
-  const resultString = wrapButtonContent(formatContent(context, element))
-  return {
-    strings: [resultString],
-    haveSimpleHorizontalGroup: false,
-  }
-}
 
 export function exportButtonGroupContentToStructure(
   context: ConfigurationContext,
@@ -36,4 +25,3 @@ registerMetadata(
   "ButtonGroup",
   exportButtonGroupContentToStructure as ExportToStructureContentFn
 )
-registerMetadata("ExportToStructure", "ButtonGroup", exportButtonGroupToStructure as ExportToStructureFn)
