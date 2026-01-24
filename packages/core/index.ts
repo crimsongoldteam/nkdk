4a3987577e78263ec79f53fe329d7de2c9c27122
nikita <nikita@crimsongold.ru>
2026-01-24 22:48:31 +0500

    refactor: remove unused FormMetadataXML type export from core index file for cleaner code

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -17,7 +17,7 @@ export {
   importCatalogFormFromXML,
   importChildItemsFromStructure,
 } from "./metadata/forms/index.js"
-export type { CatalogFormEnterprise, CatalogFormXML, FormMetadataXML } from "./metadata/forms/index.js"
+export type { CatalogFormEnterprise, CatalogFormXML } from "./metadata/forms/index.js"
 export { xmlExport } from "./xml/export/exporter.js"
 export { importContentFromXML } from "./xml/import/importer.js"
 export { exportToYAML } from "./yaml/export.js"
