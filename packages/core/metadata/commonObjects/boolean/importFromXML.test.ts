fb3fcb4a09ba0b2aa9a26c5be5e7d77f0ba2e4c2
nikita <nikita@crimsongold.ru>
2026-02-03 23:38:40 +0500

    remove wrong files

--- packages/core/metadata/commonObjects/boolean/importFromXML.test.ts
+++ packages/core/metadata/commonObjects/boolean/importFromXML.test.ts
@@ -1,34 +1,34 @@
 import { describe, expect, it } from "vitest"
 import { mockContext, mockRule } from "../../../tests/mockContext"
-import { _importBooleanFromXML } from "./_importFromXML"
+import { importBooleanFromXML } from "./importFromXML"
 
 describe("importBooleanFromXML", () => {
   it("should return undefined when xml is undefined", () => {
-    const result = _importBooleanFromXML(mockContext, mockRule, undefined)
+    const result = importBooleanFromXML(mockContext, mockRule, undefined)
 
     expect(result).toBeUndefined()
   })
 
   it("should return true when xml is 'true'", () => {
-    const result = _importBooleanFromXML(mockContext, mockRule, "true")
+    const result = importBooleanFromXML(mockContext, mockRule, "true")
 
     expect(result).toBe(true)
   })
 
   it("should return false when xml is 'false'", () => {
-    const result = _importBooleanFromXML(mockContext, mockRule, "false")
+    const result = importBooleanFromXML(mockContext, mockRule, "false")
 
     expect(result).toBe(false)
   })
 
   it("should return true when xml is boolean true", () => {
-    const result = _importBooleanFromXML(mockContext, mockRule, true)
+    const result = importBooleanFromXML(mockContext, mockRule, true)
 
     expect(result).toBe(true)
   })
 
   it("should return false when xml is boolean false", () => {
-    const result = _importBooleanFromXML(mockContext, mockRule, false)
+    const result = importBooleanFromXML(mockContext, mockRule, false)
 
     expect(result).toBe(false)
   })
--- packages/core/metadata/commonObjects/rules.ts
+++ packages/core/metadata/commonObjects/rules.ts
@@ -3,111 +3,104 @@
 // import { exportSystemEnumerationToYAML } from "../systemEnumerations/exportToEnterprise"
 // import { exportSystemEnumerationToPreview } from "../systemEnumerations/exportToPreview"
 // import { importSystemEnumerationFromYAML } from "../systemEnumerations/importFromEnterprise"
-// import { _importBooleanFromXML } from "./boolean/_importFromXML"
+// import { _importBooleanFromXML } from "./boolean/importFromXML"
 // import { exportBooleanToYAML } from "./boolean/exportToYAML"
-// import { _exportBorderToXML } from "./border/_exportToXML"
-// import { _importBorderFromXML } from "./border/_importFromXML"
+// import { _exportBorderToXML } from "./border/exportToXML"
+// import { _importBorderFromXML } from "./border/importFromXML"
 // import { exportBorderToYAML } from "./border/exportToYAML"
 // import { importBorderFromYAML } from "./border/importFromYAML"
-// import { _exportColorToXML } from "./color/_exportToXML"
-// import { _importColorFromXML } from "./color/_importFromXML"
+// import { _exportColorToXML } from "./color/exportToXML"
+// import { _importColorFromXML } from "./color/importFromXML"
 // import { exportColorToYAML } from "./color/exportToYAML"
 // import { importColorFromYAML } from "./color/importFromYAML"
 // import { exportDynamicListToYAML } from "./dynamicList/exportToYAML"
-// import { _importFieldsListFromXML } from "./fieldsList/_importFromXML"
+// import { _importFieldsListFromXML } from "./fieldsList/importFromXML"
 // import { exportFieldsListToYAML } from "./fieldsList/exportToYAML"
-// import { _importFontFromXML } from "./font/_importFromXML"
+// import { _importFontFromXML } from "./font/importFromXML"
 // import { exportFontToEnterprise } from "./font/exportToEnterprise"
 // import { exportFontToPreview } from "./font/exportToPreview"
 // import { importFontFromYAML } from "./font/importFromYAML"
-// import { exportFormattedI8nTextToXML } from "./formattedI8nText/_exportToXML"
-// import { _importFormattedI8nTextFromXML } from "./formattedI8nText/_importFromXML"
+// import { exportFormattedI8nTextToXML } from "./formattedI8nText/exportToXML"
+// import { _importFormattedI8nTextFromXML } from "./formattedI8nText/importFromXML"
 // import { exportFormattedI8nTextToYAML } from "./formattedI8nText/exportToYAML"
 // import { importFormattedI8nTextFromYAML } from "./formattedI8nText/importFromYAML"
-// import { _exportFormParametersToXML } from "./formParameter/_exportToXML"
-// import { _importFormParametersFromXML } from "./formParameter/_importFromXML"
+// import { _exportFormParametersToXML } from "./formParameter/exportToXML"
+// import { _importFormParametersFromXML } from "./formParameter/importFromXML"
 // import { exportFormParametersToYAML } from "./formParameter/exportToYAML"
 // import { importFormParametersFromYAML } from "./formParameter/importFromYAML"
-// import { _exportFunctionalOptionsToXML } from "./functionalOptionsProperty/_exportToXML"
-// import { _importFunctionalOptionsFromXML } from "./functionalOptionsProperty/_importFromXML"
+// import { _exportFunctionalOptionsToXML } from "./functionalOptionsProperty/exportToXML"
+// import { _importFunctionalOptionsFromXML } from "./functionalOptionsProperty/importFromXML"
 // import { exportFunctionalOptionsToYAML } from "./functionalOptionsProperty/exportToYAML"
 // import { importFunctionalOptionsFromYAML } from "./functionalOptionsProperty/importFromYAML"
-// import { _exportI8nTextToXML } from "./i8nText/_exportToXML"
-// import { _importI8nTextFromXML } from "./i8nText/_importFromXML"
+// import { _exportI8nTextToXML } from "./i8nText/exportToXML"
+// import { _importI8nTextFromXML } from "./i8nText/importFromXML"
 // import { exportI8nTextToEnterprise } from "./i8nText/exportToEnterprise"
 // import { exportI8nTextToPreview } from "./i8nText/exportToPreview"
 // import { importI8nTextFromYAML } from "./i8nText/importFromYAML"
-// import { _exportIndexFieldToXML } from "./indexField/_exportToXML"
-// import { _importIndexFieldFromXML } from "./indexField/_importFromXML"
+// import { _exportIndexFieldToXML } from "./indexField/exportToXML"
+// import { _importIndexFieldFromXML } from "./indexField/importFromXML"
 // import { exportIndexFieldToYAML } from "./indexField/exportToYAML"
 // import { importIndexFieldFromYAML } from "./indexField/importFromYAML"
-// import { _exportMetadataFieldToXML } from "./metadataField/_exportToXML"
-// import { _importMetadataFieldFromXML } from "./metadataField/_importFromXML"
+// import { _exportMetadataFieldToXML } from "./metadataField/exportToXML"
+// import { _importMetadataFieldFromXML } from "./metadataField/importFromXML"
 // import { exportMetadataFieldToYAML } from "./metadataField/exportToYAML"
 // import { importMetadataFieldFromYAML } from "./metadataField/importFromYAML"
-// import { _exportMetadataValueToXML } from "./metadataValue/_exportToXML"
-// import { importMetadataValueFromXML } from "./metadataValue/_importFromXML"
+// import { _exportMetadataValueToXML } from "./metadataValue/exportToXML"
+// import { importMetadataValueFromXML } from "./metadataValue/importFromXML"
 // import { exportMetadataValueToYAML } from "./metadataValue/exportToYAML"
 // import { importMetadataValueFromYAML } from "./metadataValue/importFromYAML"
-// import { _exportMetadataValueCollectionToXML } from "./metadataValueCollection/_exportToXML"
-// import { _importMetadataValueCollectionFromXML } from "./metadataValueCollection/_importFromXML"
+// import { _exportMetadataValueCollectionToXML } from "./metadataValueCollection/exportToXML"
+// import { _importMetadataValueCollectionFromXML } from "./metadataValueCollection/importFromXML"
 // import { importMetadataValueCollectionFromYAML } from "./metadataValueCollection/importFromYAML"
 // import { exportPictureToYAML } from "./picture/exportToYAML"
-// import { _exportPredefinedToXML } from "./predifined/_exportToXML"
-// import { _importPredefinedFromXML } from "./predifined/_importFromXML"
+// import { _exportPredefinedToXML } from "./predifined/exportToXML"
+// import { _importPredefinedFromXML } from "./predifined/importFromXML"
 // import { exportPredefinedToYAML } from "./predifined/exportToYAML"
 // import { importPredefinedFromYAML } from "./predifined/importFromYAML"
-// import { _exportTypeDescriptionToXML } from "./typeDescription/_exportToXML"
+// import { _exportTypeDescriptionToXML } from "./typeDescription/exportToXML"
 // import { exportTypeDescriptionToYAML } from "./typeDescription/exportToYAML"
-// import { _exportTypeLinkToXML } from "./typeLink/_exportToXML"
-// import { _importTypeLinkFromXML } from "./typeLink/_importFromXML"
+// import { _exportTypeLinkToXML } from "./typeLink/exportToXML"
+// import { _importTypeLinkFromXML } from "./typeLink/importFromXML"
 // import { exportTypeLinkToYAML } from "./typeLink/exportToYAML"
 // import { importTypeLinkFromYAML } from "./typeLink/importFromYAML"
-// import { _exportUsePurposesToXML } from "./usePurposes/_exportToXML"
-// import { _importUsePurposesFromXML } from "./usePurposes/_importFromXML"
+// import { _exportUsePurposesToXML } from "./usePurposes/exportToXML"
+// import { _importUsePurposesFromXML } from "./usePurposes/importFromXML"
 // import { exportUsePurposesToYAML } from "./usePurposes/exportToYAML"
 // import { importUsePurposesFromYAML } from "./usePurposes/importFromYAML"
-// import { _exportUserVisibleToXML } from "./userVisible/_exportToXML"
-// import { _importUserVisibleFromXML } from "./userVisible/_importFromXML"
+// import { _exportUserVisibleToXML } from "./userVisible/exportToXML"
+// import { _importUserVisibleFromXML } from "./userVisible/importFromXML"
 // import { exportUserVisibleToYAML } from "./userVisible/exportToYAML"
 // import { importUserVisibleFromYAML } from "./userVisible/importFromYAML"
-// import { _exportChoiceParametersToXML } from "./сhoiceParameters/_exportToXML"
-// import { _importChoiceParametersFromXML } from "./сhoiceParameters/_importFromXML"
+// import { _exportChoiceParametersToXML } from "./сhoiceParameters/exportToXML"
+// import { _importChoiceParametersFromXML } from "./сhoiceParameters/importFromXML"
 // import { exportChoiceParametersToYAML } from "./сhoiceParameters/exportToYAML"
 // import { importChoiceParametersFromYAML } from "./сhoiceParameters/importFromYAML"
 
-// interface TypeRule {
-//   importFromXML?: (
-//     context: ConfigurationContext,
-//     _rule: PropertyRule | undefined,
-//     rule: PropertyRule,
-//     data: any | undefined,
-//     ...args: any[]
-//   ) => any | undefined
-//   exportToXML?: (context: ConfigurationContext, _rule: PropertyRule | undefined, data: any, rule?: PropertyRule) => any
-//   importFromYAML?: (
-//     context: ConfigurationContext,
-//     _rule: PropertyRule | undefined,
-//     rule: PropertyRule,
-//     data: any
-//   ) => any
-//   exportToYAML?: (context: ConfigurationContext, _rule: PropertyRule | undefined, rule: PropertyRule, data: any) => any
-//   exportToPreview?: (
-//     context: ConfigurationContext,
-//     _rule: PropertyRule | undefined,
-//     rule: PropertyRule,
-//     data: any
-//   ) => any
-// }
+import { ConfigurationContext } from "../context/types"
+import { PropertyRule } from "../forms/elements/calendarField/rules"
+import { importBooleanFromXML } from "./boolean/importFromXML"
+import { exportBooleanToYAML } from "./boolean/exportToYAML"
 
-// export const TypeRules: Record<string, TypeRule[]> = {
-//   boolean: [
-//     {
-//       importFromXML: _importBooleanFromXML,
-//       importFromYAML: exportBooleanFromYAML,
-//       exportToYAML: exportBooleanToYAML,
-//     },
-//   ],
+export interface TypeRule {
+  importFromXML?: (
+    context: ConfigurationContext,
+    rule: PropertyRule | undefined,
+    data: any | undefined
+  ) => any | undefined
+  exportToXML?: (context: ConfigurationContext, rule: PropertyRule | undefined, data: any) => any
+  importFromYAML?: (context: ConfigurationContext, rule: PropertyRule | undefined, data: any) => any
+  exportToYAML?: (context: ConfigurationContext, rule: PropertyRule | undefined, data: any) => any
+  exportToPreview?: (context: ConfigurationContext, rule: PropertyRule | undefined, data: any) => any
+}
+
+export const TypeRules: Record<string, TypeRule[]> = {
+  boolean: [
+    {
+      importFromXML: importBooleanFromXML,
+      importFromYAML: exportBooleanFromYAML,
+      exportToYAML: exportBooleanToYAML,
+    },
+  ],
 //   Border: [
 //     {
 //       importFromXML: _importBorderFromXML,
