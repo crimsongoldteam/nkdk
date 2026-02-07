349b3e7cead10cf0a18b0a458ed0582ae57fbf99
nikita <nikita@crimsongold.ru>
2026-02-07 09:59:36 +0500

    refactor(metadata): consolidate form element export/import logic using centralized factory functions
    - Replaced large manual implementations with delegating calls to centralized factory methods (exportElementToEnterprisePartial, exportElementToXML, importElementFromEnterprisePartial, importElementFromXML)
    - Removed duplicate code across button, checkBoxField, and inputField elements (over 2000 lines reduced)
    - Simplified exportToEnterprise, exportToXML, importFromEnterprise, and importFromXML functions for each element type
    - Removed typed export/import functions in favor of partial implementations using the metadata factory

--- packages/core/metadata/forms/elements/button/exportToXML.ts
+++ packages/core/metadata/forms/elements/button/exportToXML.ts
@@ -1,113 +1,15 @@
-import { exportColorToXML } from "~/metadata/commonObjects/color/exportToXML"
-import { exportFontToXML } from "~/metadata/commonObjects/font/exportToXML"
-import { exportI8nTextToXMLWithDefaultLanguage } from "~/metadata/commonObjects/i8nText/exportToXML"
-import { exportPictureToXML } from "~/metadata/commonObjects/picture/exportToXML"
-import { exportUserVisibleToXML } from "~/metadata/commonObjects/userVisible/exportToXML"
 import { ConfigurationContext } from "~/metadata/context/types"
-import { exportElementPropsToXML } from "~/metadata/forms/elements/baseElement/exportToXML"
-import { Button, ButtonXML } from "~/metadata/forms/elements/button/types"
-import { sortObject } from "~/metadata/helpers/compactObject"
-import { registerMetadata } from "~/metadata/metadataFactory/metadataFactory"
+import { Button } from "~/metadata/forms/elements/button/types"
+import { exportElementToXML, registerMetadata } from "~/metadata/metadataFactory"
 import { ExportToXMLFn, ToXMLType } from "~/metadata/metadataFactory/types"
 import { PropertyRule } from "../calendarField/rules"
-import { exportExtendedTooltipToDeprecatedXML } from "../extendedTooltip/exportToXML"
 
 export function exportButtonToXML<From extends Button | undefined>(
   context: ConfigurationContext,
   _rule: PropertyRule | undefined,
   data: From
 ): ToXMLType<From> {
-  if (data === undefined) return undefined as ToXMLType<From>
-
-  const baseFields = exportElementPropsToXML(context, undefined, data)
-
-  const extendedTooltip = exportExtendedTooltipToDeprecatedXML(context, undefined, data.extendedTooltip, data)
-
-  const result: ButtonXML = {
-    ...baseFields,
-    ExtendedTooltip: extendedTooltip,
-  }
-
-  if (data.autoMaxHeight !== undefined) result.AutoMaxHeight = data.autoMaxHeight
-
-  if (data.autoMaxWidth !== undefined) result.AutoMaxWidth = data.autoMaxWidth
-
-  const backColor = exportColorToXML(context, undefined, data.backColor)
-  if (backColor !== undefined) result.BackColor = backColor
-
-  const borderColor = exportColorToXML(context, undefined, data.borderColor)
-  if (borderColor !== undefined) result.BorderColor = borderColor
-
-  if (data.commandName !== undefined) result.CommandName = data.commandName
-
-  if (data.commandUniqueness !== undefined) result.CommandUniqueness = data.commandUniqueness
-
-  if (data.dataPath !== undefined) result.DataPath = data.dataPath
-
-  if (data.defaultButton !== undefined) result.DefaultButton = data.defaultButton
-
-  if (data.defaultItem !== undefined) result.DefaultItem = data.defaultItem
-
-  if (data.displayImportance !== undefined) result._DisplayImportance = data.displayImportance
-
-  if (data.enabled !== undefined) result.Enabled = data.enabled
-
-  const font = exportFontToXML(context, undefined, data.font)
-  if (font !== undefined) result.Font = font
-
-  if (data.height !== undefined) result.Height = data.height
-
-  if (data.horizontalAlignInGroup !== undefined) result.GroupHorizontalAlign = data.horizontalAlignInGroup
-
-  if (data.horizontalStretch !== undefined) result.HorizontalStretch = data.horizontalStretch
-
-  if (data.locationInCommandBar !== undefined) result.LocationInCommandBar = data.locationInCommandBar
-
-  if (data.maxHeight !== undefined) result.MaxHeight = data.maxHeight
-
-  if (data.maxWidth !== undefined) result.MaxWidth = data.maxWidth
-
-  if (data.onlyInAllActions !== undefined) result.OnlyInAllActions = data.onlyInAllActions
-
-  const picture = exportPictureToXML(context, undefined, data.picture)
-  if (picture !== undefined) result.Picture = picture
-
-  if (data.pictureLocation !== undefined) result.PictureLocation = data.pictureLocation
-
-  if (data.representation !== undefined) result.Representation = data.representation
-
-  if (data.shape !== undefined) result.Shape = data.shape
-
-  if (data.shapeRepresentation !== undefined) result.ShapeRepresentation = data.shapeRepresentation
-
-  if (data.shortcut !== undefined) result.Shortcut = data.shortcut
-
-  if (data.skipOnInput !== undefined) result.SkipOnInput = data.skipOnInput
-
-  const textColor = exportColorToXML(context, undefined, data.textColor)
-  if (textColor !== undefined) result.TextColor = textColor
-
-  const title = exportI8nTextToXMLWithDefaultLanguage(context, undefined, data.title)
-  if (title !== undefined) result.Title = title
-
-  if (data.titleHeight !== undefined) result.TitleHeight = data.titleHeight
-
-  if (data.toolTipRepresentation !== undefined) result.ToolTipRepresentation = data.toolTipRepresentation
-
-  if (data.type !== undefined) result.Type = data.type
-
-  const userVisible = exportUserVisibleToXML(context, undefined, data.userVisible)
-  if (userVisible !== undefined) result.UserVisible = userVisible
-
-  if (data.verticalAlignInGroup !== undefined) result.GroupVerticalAlign = data.verticalAlignInGroup
-
-  if (data.verticalStretch !== undefined) result.VerticalStretch = data.verticalStretch
-
-  if (data.visible !== undefined) result.Visible = data.visible
-
-  if (data.width !== undefined) result.Width = data.width
-
-  return sortObject(result) as ToXMLType<From>
+  return exportElementToXML(context, "Button", data) as ToXMLType<From>
 }
 
 registerMetadata("ExportToXML", "Button", exportButtonToXML as ExportToXMLFn)
--- packages/core/metadata/forms/elements/button/importFromXML.ts
+++ packages/core/metadata/forms/elements/button/importFromXML.ts
@@ -1,13 +1,6 @@
-import { importColorFromXML } from "~/metadata/commonObjects/color/importFromXML"
-import { importFontFromXML } from "~/metadata/commonObjects/font/importFromXML"
-import { importI8nTextFromXML } from "~/metadata/commonObjects/i8nText/importFromXML"
-import { importPictureFromXML } from "~/metadata/commonObjects/picture/importFromXML"
-import { importUserVisibleFromXML } from "~/metadata/commonObjects/userVisible/importFromXML"
 import { ConfigurationContext } from "~/metadata/context/types"
-import { importBaseElementFromXML } from "~/metadata/forms/elements/baseElement/importFromXML"
 import { Button } from "~/metadata/forms/elements/button/types"
-import { importExtendedTooltipFromXML } from "~/metadata/forms/elements/extendedTooltip/importFromXML"
-import { registerMetadata } from "~/metadata/metadataFactory/metadataFactory"
+import { importElementFromXML, registerMetadata } from "~/metadata/metadataFactory"
 import { FormElementType, ImportFromXMLFn, ToXMLType } from "~/metadata/metadataFactory/types"
 import { PropertyRule } from "../calendarField/rules"
 
@@ -16,98 +9,7 @@ export function importButtonFromXML<To extends Button | undefined>(
   _rule: PropertyRule | undefined,
   xml: ToXMLType<To> | undefined
 ): To {
-  if (xml === undefined) return undefined as To
-
-  const baseFields = importBaseElementFromXML(context, undefined, xml)
-
-  const result: Button = {
-    ...baseFields,
-    elementType: FormElementType.Button,
-  }
-
-  if (xml.AutoMaxHeight !== undefined) result.autoMaxHeight = xml.AutoMaxHeight
-
-  if (xml.AutoMaxWidth !== undefined) result.autoMaxWidth = xml.AutoMaxWidth
-
-  const backColor = importColorFromXML(context, undefined, xml.BackColor)
-  if (backColor !== undefined) result.backColor = backColor
-
-  const borderColor = importColorFromXML(context, undefined, xml.BorderColor)
-  if (borderColor !== undefined) result.borderColor = borderColor
-
-  if (xml.CommandName !== undefined) result.commandName = xml.CommandName
-
-  if (xml.CommandUniqueness !== undefined) result.commandUniqueness = xml.CommandUniqueness
-
-  if (xml.DataPath !== undefined) result.dataPath = xml.DataPath
-
-  if (xml.DefaultButton !== undefined) result.defaultButton = xml.DefaultButton
-
-  if (xml.DefaultItem !== undefined) result.defaultItem = xml.DefaultItem
-
-  if (xml._DisplayImportance !== undefined) result.displayImportance = xml._DisplayImportance
-
-  if (xml.Enabled !== undefined) result.enabled = xml.Enabled
-
-  const extendedTooltip = importExtendedTooltipFromXML(context, undefined, xml.ExtendedTooltip)
-  if (extendedTooltip !== undefined) result.extendedTooltip = extendedTooltip
-
-  const font = importFontFromXML(context, undefined, xml.Font)
-  if (font !== undefined) result.font = font
-
-  if (xml.Height !== undefined) result.height = xml.Height
-
-  if (xml.GroupHorizontalAlign !== undefined) result.horizontalAlignInGroup = xml.GroupHorizontalAlign
-
-  if (xml.HorizontalStretch !== undefined) result.horizontalStretch = xml.HorizontalStretch
-
-  if (xml.LocationInCommandBar !== undefined) result.locationInCommandBar = xml.LocationInCommandBar
-
-  if (xml.MaxHeight !== undefined) result.maxHeight = xml.MaxHeight
-
-  if (xml.MaxWidth !== undefined) result.maxWidth = xml.MaxWidth
-
-  if (xml.OnlyInAllActions !== undefined) result.onlyInAllActions = xml.OnlyInAllActions
-
-  const picture = importPictureFromXML(context, undefined, xml.Picture)
-  if (picture !== undefined) result.picture = picture
-
-  if (xml.PictureLocation !== undefined) result.pictureLocation = xml.PictureLocation
-
-  if (xml.Representation !== undefined) result.representation = xml.Representation
-
-  if (xml.Shape !== undefined) result.shape = xml.Shape
-
-  if (xml.ShapeRepresentation !== undefined) result.shapeRepresentation = xml.ShapeRepresentation
-
-  if (xml.Shortcut !== undefined) result.shortcut = xml.Shortcut
-
-  if (xml.SkipOnInput !== undefined) result.skipOnInput = xml.SkipOnInput
-
-  const textColor = importColorFromXML(context, undefined, xml.TextColor)
-  if (textColor !== undefined) result.textColor = textColor
-
-  const title = importI8nTextFromXML(context, undefined, xml.Title)
-  if (title !== undefined) result.title = title
-
-  if (xml.TitleHeight !== undefined) result.titleHeight = xml.TitleHeight
-
-  if (xml.ToolTipRepresentation !== undefined) result.toolTipRepresentation = xml.ToolTipRepresentation
-
-  if (xml.Type !== undefined) result.type = xml.Type
-
-  const userVisible = importUserVisibleFromXML(context, undefined, xml.UserVisible)
-  if (userVisible !== undefined) result.userVisible = userVisible
-
-  if (xml.GroupVerticalAlign !== undefined) result.verticalAlignInGroup = xml.GroupVerticalAlign
-
-  if (xml.VerticalStretch !== undefined) result.verticalStretch = xml.VerticalStretch
-
-  if (xml.Visible !== undefined) result.visible = xml.Visible
-
-  if (xml.Width !== undefined) result.width = xml.Width
-
-  return result as To
+  return importElementFromXML(context, FormElementType.Button, xml) as To
 }
 
 registerMetadata("ImportFromXML", "Button", importButtonFromXML as ImportFromXMLFn)
