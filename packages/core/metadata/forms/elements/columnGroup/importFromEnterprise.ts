716d876af9a285acf3a9b109feb87a9d90a4d2c8
nikita <nikita@crimsongold.ru>
2026-02-07 10:58:42 +0500

    refactor(metadata/forms): consolidate ChartField and ColumnGroup import logic using centralized factory functions
    Replace verbose per-element import implementations with centralized factory functions for ChartField and ColumnGroup elements. Removed individual import statements and eliminated large helper functions (importChartFieldPropsFromEnterprise, importColumnGroupPropsFromEnterprise) in favor of generic importElementFromEnterpriseTyped, importElementFromEnterprisePartial, and importElementFromXML functions. This follows the existing pattern established for other form elements and reduces code duplication while maintaining consistent import behavior across all form element types.

--- packages/core/metadata/forms/elements/columnGroup/importFromEnterprise.ts
+++ packages/core/metadata/forms/elements/columnGroup/importFromEnterprise.ts
@@ -1,213 +1,23 @@
-import { importBooleanFromEnterprise } from "~/metadata/commonObjects/boolean/importFromEnterprise"
-import { importColorFromEnterprise } from "~/metadata/commonObjects/color/importFromEnterprise"
-import { importFontFromEnterprise } from "~/metadata/commonObjects/font/importFromEnterprise"
-import {
-  importI8nTextCombinedFromEnterprise,
-  importI8nTextFromEnterprise,
-} from "~/metadata/commonObjects/i8nText/importFromEnterprise"
-import { importPictureFromEnterprise } from "~/metadata/commonObjects/picture/importFromEnterprise"
-import { importUserVisibleFromEnterprise } from "~/metadata/commonObjects/userVisible/importFromEnterprise"
 import { ConfigurationContext } from "~/metadata/context/types"
-import {
-  ColumnGroup,
-  ColumnGroupPartialEnterprise,
-  ColumnGroupTypedEnterprise,
-} from "~/metadata/forms/elements/columnGroup/types"
-import { registerMetadata } from "~/metadata/metadataFactory/metadataFactory"
-import {
-  ImportPartialFromEnterpriseFn,
-  ImportTypedFromEnterpriseFn,
-  ToPartialEnterpriseType,
-  ToTypedEnterpriseType,
-} from "~/metadata/metadataFactory/types"
-import { importSystemEnumerationFromEnterprise } from "~/metadata/systemEnumerations/importFromEnterprise"
-import * as SE from "~/metadata/systemEnumerations/types"
-import { importChildItemsTypedFromEnterprise } from "../../collections/childItems/importFromEnterprise"
-import { PropertyRule } from "../calendarField/rules"
-import { importExtendedTooltipFromEnterprise } from "../extendedTooltip/importFromEnterprise"
+import { ColumnGroup } from "~/metadata/forms/elements/columnGroup/types"
+import { importElementFromEnterprisePartial, importElementFromEnterpriseTyped, registerMetadata } from "~/metadata/metadataFactory"
+import { FormElementType, ToPartialEnterpriseType, ToTypedEnterpriseType } from "~/metadata/metadataFactory/types"
+
 export function importColumnGroupTypedFromEnterprise<To extends ColumnGroup | undefined>(
   context: ConfigurationContext,
-  _rule: PropertyRule | undefined,
   data: ToTypedEnterpriseType<To>,
   name: string
 ): To {
-  if (data === undefined) return undefined as To
-
-  const props = importColumnGroupPropsFromEnterprise(context, undefined, data)
-
-  const result: ColumnGroup = {
-    ...props,
-    elementType: "ColumnGroup",
-    name,
-    childItems: props.childItems ?? [],
-  }
-
-  const title = importI8nTextFromEnterprise(context, undefined, data.Заголовок)
-  if (title !== undefined) result.title = title
-
-  return result as To
+  return importElementFromEnterpriseTyped(context, FormElementType.ColumnGroup, data as any, name) as To
 }
 
 export function importColumnGroupPartialFromEnterprise<To extends ColumnGroup>(
   context: ConfigurationContext,
-  _rule: PropertyRule | undefined,
   source: To,
   data: ToPartialEnterpriseType<To> | undefined
 ): To {
-  const props = importColumnGroupPropsFromEnterprise(context, undefined, data)
-  const result: To = {
-    ...source,
-    ...props,
-    childItems: props.childItems ?? [],
-  }
-
-  const title = importI8nTextCombinedFromEnterprise(context, undefined, source.title, data?.Заголовок)
-  if (title !== undefined) result.title = title
-
-  return result
+  return importElementFromEnterprisePartial(context, FormElementType.ColumnGroup, source, data as any)
 }
 
-const importColumnGroupPropsFromEnterprise = (
-  context: ConfigurationContext,
-  _rule: PropertyRule | undefined,
-  data: ColumnGroupTypedEnterprise | ColumnGroupPartialEnterprise | undefined
-): Omit<Partial<ColumnGroup>, "elementType" | "name"> => {
-  const result: Omit<Partial<ColumnGroup>, "elementType" | "name"> = {
-    childItems: [],
-  }
-
-  if (data === undefined) return result
-
-  const verticalAlignInGroup = importSystemEnumerationFromEnterprise<SE.ItemVerticalAlign>(
-    context,
-    undefined,
-    data.ВертикальноеПоложениеВГруппе,
-    SE.ItemVerticalAlignFromEnterprise
-  )
-  if (verticalAlignInGroup !== undefined) result.verticalAlignInGroup = verticalAlignInGroup
-
-  const type = importSystemEnumerationFromEnterprise<SE.FormGroupType>(
-    context,
-    undefined,
-    data.Вид,
-    SE.FormGroupTypeFromEnterprise
-  )
-  if (type !== undefined) result.type = type
-
-  const visible = importBooleanFromEnterprise(context, undefined, data.Видимость)
-  if (visible !== undefined) result.visible = visible
-
-  if (data.Высота !== undefined) result.height = data.Высота
-
-  const horizontalAlignInGroup = importSystemEnumerationFromEnterprise<SE.ItemHorizontalLocation>(
-    context,
-    undefined,
-    data.ГоризонтальноеПоложениеВГруппе,
-    SE.ItemHorizontalLocationFromEnterprise
-  )
-  if (horizontalAlignInGroup !== undefined) result.horizontalAlignInGroup = horizontalAlignInGroup
-
-  const enabled = importBooleanFromEnterprise(context, undefined, data.Доступность)
-  if (enabled !== undefined) result.enabled = enabled
-
-  const toolTipRepresentation = importSystemEnumerationFromEnterprise<SE.ToolTipRepresentation>(
-    context,
-    undefined,
-    data.ОтображениеПодсказки,
-    SE.ToolTipRepresentationFromEnterprise
-  )
-  if (toolTipRepresentation !== undefined) result.toolTipRepresentation = toolTipRepresentation
-
-  const toolTip = importI8nTextFromEnterprise(context, undefined, data.Подсказка)
-  if (toolTip !== undefined) result.toolTip = toolTip
-
-  const enableContentChange = importBooleanFromEnterprise(context, undefined, data.РазрешитьИзменениеСостава)
-  if (enableContentChange !== undefined) result.enableContentChange = enableContentChange
-
-  const verticalStretch = importBooleanFromEnterprise(context, undefined, data.РастягиватьПоВертикали)
-  if (verticalStretch !== undefined) result.verticalStretch = verticalStretch
-
-  const horizontalStretch = importBooleanFromEnterprise(context, undefined, data.РастягиватьПоГоризонтали)
-  if (horizontalStretch !== undefined) result.horizontalStretch = horizontalStretch
-
-  const extendedTooltip = importExtendedTooltipFromEnterprise(context, undefined, data.РасширеннаяПодсказка)
-  if (extendedTooltip !== undefined) result.extendedTooltip = extendedTooltip
-
-  if (data.СочетаниеКлавиш !== undefined) result.shortcut = data.СочетаниеКлавиш
-
-  const readOnly = importBooleanFromEnterprise(context, undefined, data.ТолькоПросмотр)
-  if (readOnly !== undefined) result.readOnly = readOnly
-
-  const titleTextColor = importColorFromEnterprise(context, undefined, data.ЦветТекстаЗаголовка)
-  if (titleTextColor !== undefined) result.titleTextColor = titleTextColor
-
-  if (data.Ширина !== undefined) result.width = data.Ширина
-
-  const titleFont = importFontFromEnterprise(context, undefined, data.ШрифтЗаголовка)
-  if (titleFont !== undefined) result.titleFont = titleFont
-
-  const headerHorizontalAlign = importSystemEnumerationFromEnterprise<SE.ItemHorizontalLocation>(
-    context,
-    undefined,
-    data.ГоризонтальноеПоложениеВШапке,
-    SE.ItemHorizontalLocationFromEnterprise
-  )
-  if (headerHorizontalAlign !== undefined) result.headerHorizontalAlign = headerHorizontalAlign
-
-  const group = importSystemEnumerationFromEnterprise<SE.ColumnsGroup>(
-    context,
-    undefined,
-    data.Группировка,
-    SE.ColumnsGroupFromEnterprise
-  )
-  if (group !== undefined) result.group = group
-
-  const headerPicture = importPictureFromEnterprise(context, undefined, data.КартинкаШапки)
-  if (headerPicture !== undefined) result.headerPicture = headerPicture
-
-  const showInHeader = importBooleanFromEnterprise(context, undefined, data.ОтображатьВШапке)
-  if (showInHeader !== undefined) result.showInHeader = showInHeader
-
-  const showTitle = importBooleanFromEnterprise(context, undefined, data.ОтображатьЗаголовок)
-  if (showTitle !== undefined) result.showTitle = showTitle
-
-  const userVisible = importUserVisibleFromEnterprise(
-    context,
-    undefined,
-    data.РазрешитьИспользование,
-    data.ЗапретитьИспользование
-  )
-  if (userVisible !== undefined) {
-    result.userVisible = userVisible
-  }
-
-  if (data.ПутьКДаннымШапки !== undefined) result.headerDataPath = data.ПутьКДаннымШапки
-
-  const fixingInTable = importSystemEnumerationFromEnterprise<SE.FixingInTable>(
-    context,
-    undefined,
-    data.ФиксацияВТаблице,
-    SE.FixingInTableFromEnterprise
-  )
-  if (fixingInTable !== undefined) result.fixingInTable = fixingInTable
-
-  if (data.ФорматШапки !== undefined) result.headerFormat = data.ФорматШапки
-
-  const titleBackColor = importColorFromEnterprise(context, undefined, data.ЦветФонаЗаголовка)
-  if (titleBackColor !== undefined) result.titleBackColor = titleBackColor
-
-  result.childItems = importChildItemsTypedFromEnterprise(context, undefined, data?.ПодчиненныеЭлементы)
-
-  return result
-}
-
-registerMetadata(
-  "ImportPartialFromEnterprise",
-  "ColumnGroup",
-  importColumnGroupPartialFromEnterprise as ImportPartialFromEnterpriseFn
-)
-registerMetadata(
-  "ImportTypedFromEnterprise",
-  "ColumnGroup",
-  importColumnGroupTypedFromEnterprise as ImportTypedFromEnterpriseFn
-)
+registerMetadata("ImportPartialFromEnterprise", "ColumnGroup", importColumnGroupPartialFromEnterprise as any)
+registerMetadata("ImportTypedFromEnterprise", "ColumnGroup", importColumnGroupTypedFromEnterprise as any)
--- packages/core/metadata/forms/elements/columnGroup/importFromXML.ts
+++ packages/core/metadata/forms/elements/columnGroup/importFromXML.ts
@@ -1,99 +1,13 @@
-import { importColorFromXML } from "~/metadata/commonObjects/color/importFromXML"
-import { importFontFromXML } from "~/metadata/commonObjects/font/importFromXML"
-import { importI8nTextFromXML } from "~/metadata/commonObjects/i8nText/importFromXML"
-import { importPictureFromXML } from "~/metadata/commonObjects/picture/importFromXML"
-import { importUserVisibleFromXML } from "~/metadata/commonObjects/userVisible/importFromXML"
 import { ConfigurationContext } from "~/metadata/context/types"
 import { ColumnGroup } from "~/metadata/forms/elements/columnGroup/types"
-import { registerMetadata } from "~/metadata/metadataFactory/metadataFactory"
-import { ImportFromXMLFn, ToXMLType } from "~/metadata/metadataFactory/types"
-import { importChildItemsFromXML } from "../../collections/childItems/importFromXML"
-import { importBaseElementFromXML } from "../baseElement/importFromXML"
-import { importExtendedTooltipFromXML } from "../extendedTooltip/importFromXML"
-import { PropertyRule } from "../calendarField/rules"
+import { importElementFromXML, registerMetadata } from "~/metadata/metadataFactory"
+import { FormElementType, ToXMLType } from "~/metadata/metadataFactory/types"
 
 export function importColumnGroupFromXML<To extends ColumnGroup | undefined>(
   context: ConfigurationContext,
-  _rule: PropertyRule | undefined,
   xml: ToXMLType<To> | undefined
 ): To {
-  if (xml === undefined) return undefined as To
-
-  const baseFields = importBaseElementFromXML(context, undefined, xml)
-
-  const result: ColumnGroup = {
-    ...baseFields,
-    elementType: "ColumnGroup",
-    childItems: [],
-  }
-
-  if (xml.EnableContentChange !== undefined) result.enableContentChange = xml.EnableContentChange
-
-  if (xml.Enabled !== undefined) result.enabled = xml.Enabled
-
-  if (xml.Height !== undefined) result.height = xml.Height
-
-  if (xml.GroupHorizontalAlign !== undefined) result.horizontalAlignInGroup = xml.GroupHorizontalAlign
-
-  if (xml.HorizontalStretch !== undefined) result.horizontalStretch = xml.HorizontalStretch
-
-  if (xml.ReadOnly !== undefined) result.readOnly = xml.ReadOnly
-
-  if (xml.Shortcut !== undefined) result.shortcut = xml.Shortcut
-
-  const title = importI8nTextFromXML(context, undefined, xml.Title)
-  if (title !== undefined) result.title = title
-
-  const titleFont = importFontFromXML(context, undefined, xml.TitleFont)
-  if (titleFont !== undefined) result.titleFont = titleFont
-
-  const titleTextColor = importColorFromXML(context, undefined, xml.TitleTextColor)
-  if (titleTextColor !== undefined) result.titleTextColor = titleTextColor
-
-  const toolTip = importI8nTextFromXML(context, undefined, xml.ToolTip)
-  if (toolTip !== undefined) result.toolTip = toolTip
-
-  if (xml.ToolTipRepresentation !== undefined) result.toolTipRepresentation = xml.ToolTipRepresentation
-
-  if (xml.Type !== undefined) result.type = xml.Type
-
-  const extendedTooltip = importExtendedTooltipFromXML(context, undefined, xml.ExtendedTooltip)
-  if (extendedTooltip !== undefined) result.extendedTooltip = extendedTooltip
-
-  if (xml.GroupVerticalAlign !== undefined) result.verticalAlignInGroup = xml.GroupVerticalAlign
-
-  if (xml.VerticalStretch !== undefined) result.verticalStretch = xml.VerticalStretch
-
-  if (xml.Visible !== undefined) result.visible = xml.Visible
-
-  if (xml.Width !== undefined) result.width = xml.Width
-
-  if (xml.FixingInTable !== undefined) result.fixingInTable = xml.FixingInTable
-
-  if (xml.Group !== undefined) result.group = xml.Group
-
-  if (xml.HeaderDataPath !== undefined) result.headerDataPath = xml.HeaderDataPath
-
-  if (xml.HeaderFormat !== undefined) result.headerFormat = xml.HeaderFormat
-
-  if (xml.HeaderHorizontalAlign !== undefined) result.headerHorizontalAlign = xml.HeaderHorizontalAlign
-
-  const headerPicture = importPictureFromXML(context, undefined, xml.HeaderPicture)
-  if (headerPicture !== undefined) result.headerPicture = headerPicture
-
-  if (xml.ShowInHeader !== undefined) result.showInHeader = xml.ShowInHeader
-
-  if (xml.ShowTitle !== undefined) result.showTitle = xml.ShowTitle
-
-  const titleBackColor = importColorFromXML(context, undefined, xml.TitleBackColor)
-  if (titleBackColor !== undefined) result.titleBackColor = titleBackColor
-
-  const userVisible = importUserVisibleFromXML(context, undefined, xml.UserVisible)
-  if (userVisible !== undefined) result.userVisible = userVisible
-
-  result.childItems = importChildItemsFromXML(context, undefined, xml.ChildItems)
-
-  return result as To
+  return importElementFromXML<ColumnGroup>(context, FormElementType.ColumnGroup, xml) as To
 }
 
-registerMetadata("ImportFromXML", "ColumnGroup", importColumnGroupFromXML as ImportFromXMLFn)
+registerMetadata("ImportFromXML", "ColumnGroup", importColumnGroupFromXML as any)
