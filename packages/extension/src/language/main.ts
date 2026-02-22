ccf590ad3b5c6ccbdd2c33bcc74911470a2fcca1
nikita <nikita@crimsongold.ru>
2026-02-22 19:59:31 +0500

    remove CLI package and related files to streamline project structure; eliminate unused scripts and configurations for improved maintainability

--- packages/extension/src/language/main.ts
+++ packages/extension/src/language/main.ts
@@ -1,13 +1,13 @@
-import { startLanguageServer } from 'langium/lsp';
-import { NodeFileSystem } from 'langium/node';
-import { createConnection, ProposedFeatures } from 'vscode-languageserver/node.js';
-import { createNkdkServices } from 'nkdk-language';
+import { startLanguageServer } from "langium/lsp"
+import { NodeFileSystem } from "langium/node"
+import { createNkdkServices } from "nkdk-language"
+import { createConnection, ProposedFeatures } from "vscode-languageserver/node.js"
 
 // Create a connection to the client
-const connection = createConnection(ProposedFeatures.all);
+const connection = createConnection(ProposedFeatures.all)
 
 // Inject the shared services and language-specific services
-const { shared } = createNkdkServices({ connection, ...NodeFileSystem });
+const { shared } = createNkdkServices({ connection, ...NodeFileSystem })
 
 // Start the language server with the shared services
-startLanguageServer(shared);
+startLanguageServer(shared)
