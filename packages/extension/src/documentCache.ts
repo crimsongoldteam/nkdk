77cf9cdb5540f81fdc0f57a395a2dd39989113cf
nikita <nikita@crimsongold.ru>
2026-03-02 19:05:49 +0500

    Implement document caching for NKDK and YAML formats
    This commit introduces a caching mechanism for handling NKDK and YAML documents within the extension. It defines a new type, NkdkDocumentCache, to store version and form data for both document types. The ensureCache function is added to manage cache entries, while helper functions for document type identification and cache retrieval are also implemented, enhancing the overall document management process.

--- packages/extension/src/documentCache.ts
+++ packages/extension/src/documentCache.ts
@@ -1,10 +1,62 @@
 // import { TextDocument } from "vscode"
 
-// type NkdkDocumentCache = {
-//   version: number
-//   form: ClientApplicationForm
-// }
+import { ClientApplicationForm } from "@nakidka/core"
+import { TextDocument } from "vscode"
 
-// const cache = new Map<string, ClientApplicationFormYAML>()
+type NkdkDocumentCache = {
+  versionNkdk: number
+  versionYaml: number
+  formNkdk?: ClientApplicationForm
+  formYaml?: ClientApplicationForm
+}
 
-// ensureCache = (document: TextDocument) => {}
+const cache = new Map<string, NkdkDocumentCache>()
+
+export const ensureCache = (document: TextDocument) => {
+  const cacheKey = document.uri.toString()
+
+  const pairedDocumentUri = getPairedDocumentUri(document)
+
+  //   if (isYAMLDocument(document)) {
+  //     cache.versionYaml = document.version
+  //   } else {
+  //     cache.versionNkdk = document.version
+  //   }
+}
+
+const getCache = (document: TextDocument) => {
+  if (isYAMLDocument(document)) {
+    return yamlCache
+  }
+  return nkdkCache
+}
+
+const isYAMLDocument = (document: TextDocument) => {
+  return document.languageId === "yaml"
+}
+
+const getOrCreateCacheByDocument = (document: TextDocument): NkdkDocumentCache => {
+  const cacheKey = document.uri.toString()
+  if (!cache.has(cacheKey)) {
+    const newCache = {
+      versionNkdk: -1,
+      versionYaml: -1,
+      formNkdk: undefined,
+      formYaml: undefined,
+    } satisfies NkdkDocumentCache
+    cache.set(cacheKey, newCache)
+  }
+  const cacheEntry = cache.get(cacheKey)
+
+  //   const newCache = {
+  //     versionNkdk: -1,
+  //     versionYaml: -1,
+  //     formNkdk: undefined,
+  //     formYaml: undefined,
+  //   }
+  //   cache.set(cacheKey, newCache)
+  return cacheEntry
+}
+
+const getPairedDocumentUri = (document: TextDocument): string =>
+  document.uri.toString().replace(/\.(yaml|nkdk)$/i, (_, ext) => (ext.toLowerCase() === "yaml" ? ".nkdk" : ".yaml"))
