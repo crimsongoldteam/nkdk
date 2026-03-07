afce00532ea66ac702130697c531ad6db59b198e
nikita <nikita@crimsongold.ru>
2026-03-07 12:03:27 +0500

    feat: :sparkles: добавить импорт справочника из файлов в файлы

---
+++ packages/core/sync/convertCatalog.test.ts
@@ -0,0 +1,24 @@
+import fs from "fs"
+import { join } from "path"
+import { describe, expect, it, vi } from "vitest"
+import { readCatalogYAML } from "~/tests/fixtures/sync/readCatalog/data"
+import { mockContextToYAML } from "~/tests/mockContext"
+import { convertCatalogFromXML } from "./convertCatalog"
+
+describe("readCatalog", () => {
+  const inputDir = join(process.cwd(), "tests/fixtures/sync/readCatalog/Catalogs")
+  const outputDir = join(process.cwd(), "tests/fixtures/sync/out/Справочник")
+  const catalogName = "Контрагенты"
+
+  it("should read catalog from XML and export to YAML file in output dir", async () => {
+    const spy = vi.spyOn(fs, "writeFileSync").mockImplementation(() => {})
+    await convertCatalogFromXML({
+      context: mockContextToYAML,
+      inputDir: inputDir,
+      name: catalogName,
+      outputDir: outputDir,
+    })
+
+    expect(spy).toHaveBeenCalledWith(join(outputDir, catalogName, "Свойства.yaml"), readCatalogYAML, "utf-8")
+  })
+})
---
+++ packages/core/sync/convertCatalog.ts
@@ -0,0 +1,43 @@
+import fs from "fs"
+import { join } from "path"
+import { importMetadataCatalogFromXML } from "~/metadata/appliedObjects/metadataCatalog/fromXML"
+import { exportMetadataCatalogToYAML } from "~/metadata/appliedObjects/metadataCatalog/toYAML"
+import type { MetadataCatalogXML } from "~/metadata/appliedObjects/metadataCatalog/types"
+import { ConfigurationContext } from "~/metadata/context/types"
+import { importContentFromXML } from "~/xml/import/importer"
+import { exportToYAML } from "~/yaml/export"
+
+export const convertCatalogFromXML = async (params: {
+  context: ConfigurationContext
+  inputDir: string
+  name: string
+  outputDir: string
+}): Promise<void> => {
+  const { context, inputDir, name: name, outputDir } = params
+  const inputPath = join(inputDir, `${name}.xml`)
+  const xmlContent = fs.readFileSync(inputPath, "utf-8")
+
+  const result =
+    readCatalogFromXML({
+      context,
+      xml: xmlContent,
+    }) ?? ""
+
+  const outputPath = join(outputDir, name)
+  fs.mkdirSync(outputPath, { recursive: true })
+
+  const outputFilePath = join(outputPath, "Свойства.yaml")
+  fs.writeFileSync(outputFilePath, result, "utf-8")
+}
+
+export const readCatalogFromXML = (params: { context: ConfigurationContext; xml: string }): string | undefined => {
+  const { context, xml } = params
+
+  const parsed = importContentFromXML<{ MetaDataObject: MetadataCatalogXML }>(xml)
+  const catalog = importMetadataCatalogFromXML(context, parsed.MetaDataObject)
+
+  const yamlObj = exportMetadataCatalogToYAML(context, catalog)
+  const yaml = yamlObj != undefined ? exportToYAML(yamlObj) : undefined
+
+  return yaml
+}
