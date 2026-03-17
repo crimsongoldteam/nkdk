c225a41094b26383aaa3f13d36683aa278a752a1
nikita <nikita@crimsongold.ru>
2026-03-17 23:33:32 +0500

    refactor: :recycle: удалить неиспользуемые интерфейсы и свойства из ClientApplicationForm, обновить тестовые данные для полей диаграммы и планировщика, изменив значения по умолчанию

--- packages/core/metadata/orchestration/metadataItem/yaml.ts
+++ packages/core/metadata/orchestration/metadataItem/yaml.ts
@@ -54,14 +54,14 @@ type PropertiesByRule<Rule extends { properties: Record<string, PropertyRule> }>
         [K in keyof Properties as Properties[K] extends { runtimeOnly: true }
           ? never
           : Properties[K] extends { toYAML: false; fromYAML: false }
-          ? never
-          : Properties[K] extends { toPartialYAML: false }
             ? never
-            : Properties[K] extends { yaml: infer YAMLName }
-              ? YAMLName extends string
-                ? YAMLName
-                : never
-              : never]?: Properties[K] extends {
+            : Properties[K] extends { toPartialYAML: false }
+              ? never
+              : Properties[K] extends { yaml: infer YAMLName }
+                ? YAMLName extends string
+                  ? YAMLName
+                  : never
+                : never]?: Properties[K] extends {
           type: "SystemEnumeration"
           typeSE: infer TypeSE
         }
