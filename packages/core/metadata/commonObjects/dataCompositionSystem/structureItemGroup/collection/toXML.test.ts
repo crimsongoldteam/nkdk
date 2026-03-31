c49880a02c983cfcabf52e39438d7e4f09866092
nikita <nikita@crimsongold.ru>
2026-03-31 16:50:56 +0500

    refactor: :recycle: обновить тесты для экспорта структуры группировки в YAML и XML, улучшая импорты и соответствие новым стандартам обработки данных

--- packages/core/metadata/commonObjects/dataCompositionSystem/structureItemGroup/collection/toXML.test.ts
+++ packages/core/metadata/commonObjects/dataCompositionSystem/structureItemGroup/collection/toXML.test.ts
@@ -1,39 +1,31 @@
-import { dirname, join } from "node:path"
-import { fileURLToPath } from "node:url"
 import { describe, expect, it } from "vitest"
 import { testExportPropertyToXML } from "~/tests/property/exportPropertyToXML"
-import { readXMLFileAsString } from "~/tests/readAndParseXMLFile"
 import { fixtureGroupItemAuto } from "../items/groupItemAuto/__fixtures__/data"
 import { dynamicListGroupItemFieldUseFalse } from "../items/groupItemField/__fixtures__/data"
 import "./index"
 
 const rule = { type: "StructureItemGroupCollectionItem" } as const
-const fixturesDir = dirname(fileURLToPath(import.meta.url))
-const autoFixturesDir = join(fixturesDir, "../items/groupItemAuto/__fixtures__")
-const fieldFixturesDir = join(fixturesDir, "../items/groupItemField/__fixtures__")
 
 describe("export GroupItem collection to XML", () => {
   it("exports GroupItemAuto", () => {
-    const expectedResult = readXMLFileAsString("dynamicList.xml", autoFixturesDir)
-
-    const { result } = testExportPropertyToXML({
+    const { result, expectedResult } = testExportPropertyToXML({
       rule,
       value: [fixtureGroupItemAuto],
       xmlRootTag: "dcsset:item",
-      referenceMetadata: undefined,
+      path: "../../items/groupItemAuto/__fixtures__/dynamicList.xml",
+      importMetaUrl: import.meta.url,
     })
 
     expect(result).toEqual(expectedResult)
   })
 
   it("exports GroupItemField", () => {
-    const expectedResult = readXMLFileAsString("dynamicList.xml", fieldFixturesDir)
-
-    const { result } = testExportPropertyToXML({
+    const { result, expectedResult } = testExportPropertyToXML({
       rule,
       value: [dynamicListGroupItemFieldUseFalse],
       xmlRootTag: "dcsset:item",
-      referenceMetadata: undefined,
+      path: "../../items/groupItemField/__fixtures__/dynamicList.xml",
+      importMetaUrl: import.meta.url,
     })
 
     expect(result).toEqual(expectedResult)
