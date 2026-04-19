cdb4c8741c2d6442ccfdbf2140ab3feabeceb084
nikita <nikita@crimsongold.ru>
2026-04-19 12:46:50 +0500

    refactor: :recycle: убрать неиспользуемый импорт в toXML.test.ts

--- packages/core/metadata/forms/clientApplicationForm/toXML.test.ts
+++ packages/core/metadata/forms/clientApplicationForm/toXML.test.ts
@@ -2,11 +2,7 @@ import { describe, expect, it } from "vitest"
 import { mockContextFromXML, mockContextToXML } from "~/tests/mockContext"
 import { readAndParseXMLFixture, readXMLFixtureAsString } from "~/tests/readFixtureXML"
 import { xmlExport } from "~/xml/export/exporter"
-import {
-  fullClientApplicationForm,
-  minimalClientApplicationForm,
-  minimalClientApplicationFormReference,
-} from "./__fixtures__/data"
+import { fullClientApplicationForm, minimalClientApplicationForm } from "./__fixtures__/data"
 import { importClientApplicationFormFromXML } from "./fromXML"
 import { exportClientApplicationFormToXML, exportFormMetadataToXML } from "./toXML"
 import { ClientApplicationFormXML, FormMetadataXML } from "./types"
@@ -42,7 +38,10 @@ describe("exportToXML", () => {
 
     it("should export minimal", () => {
       const expectedResult = readXMLFixtureAsString(import.meta.url, "minimal.xml")
-      const referenceFormXML = readAndParseXMLFixture<{ Form: ClientApplicationFormXML }>(import.meta.url, "minimal.xml")
+      const referenceFormXML = readAndParseXMLFixture<{ Form: ClientApplicationFormXML }>(
+        import.meta.url,
+        "minimal.xml"
+      )
       const referenceMetadataXML = readAndParseXMLFixture<{ MetaDataObject: FormMetadataXML }>(
         import.meta.url,
         "minimalMetadata.xml"
