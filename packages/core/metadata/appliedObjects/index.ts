aefe6e33c216415db5f814cffdb1fbf18fc5a51a
nikita <nikita@crimsongold.ru>
2026-04-25 18:48:41 +0500

    refactor: :fire: удалить мёртвый legacy.ts и неиспользуемые конвертеры metadataDocument
    additionalIndex/legacy.ts остался от старой схемы XML/YAML. Его потребители
    (metadataDocument/fromXML.ts, toYAML.ts, toXML.ts) объявляли функции, которые
    никто не вызывал — конвертация metadataDocument уже идёт через orchestration
    по MetadataDocumentRules. Удаляю файлы и три side-effect-импорта в
    appliedObjects/index.ts.

--- packages/core/metadata/appliedObjects/index.ts
+++ packages/core/metadata/appliedObjects/index.ts
@@ -3,11 +3,6 @@ import "./metadataCommand/register"
 import "./metadataCatalog/fromYAML"
 import "./metadataCatalog/toYAML"
 
-import "./metadataDocument/toXML"
-import "./metadataDocument/toYAML"
-// import "./metadataDocument/fromYAML"
-import "./metadataDocument/fromXML"
-
 import "./metadataDocumentNumerator"
 
 import "./metadataEnumeration/fromYAML"
