cc9558ab2b9a6646c610d8dce40135495121af9f
nikita <nikita@crimsongold.ru>
2026-04-30 19:52:54 +0500

    feat: :sparkles: cypherCache в ToXMLConfigurationContext

--- packages/core/metadata/context/types.ts
+++ packages/core/metadata/context/types.ts
@@ -3,6 +3,7 @@ import { EnterpriseAttributeMapItem } from "../forms/clientApplicationForm/types
 import { FormChildItemsPartialYAML, FormElementsYAML } from "../forms/commonObjects/childItems/types"
 import { ElementType, ElementXMLWithoutId, MetadataItemType, ToMetadata } from "../orchestration"
 import { GraphBuilder } from "../orchestration/buildGraph/internal/GraphBuilder"
+import type { CypherCache } from "../orchestration/property/cypherCache"
 
 export type ContextElementToXML = {
   name: string
@@ -46,6 +47,8 @@ export type ToXMLConfigurationContext = {
   readonly configDumpInfo: ConfigDumpInfo
   readonly version: string
   readonly itemsTree: ContextElementToXML[]
+  /** Кеш результатов Cypher-запросов, заполняется до начала обхода свойств. */
+  cypherCache?: CypherCache
   context?: {
     forms: string[]
     templates: string[]
