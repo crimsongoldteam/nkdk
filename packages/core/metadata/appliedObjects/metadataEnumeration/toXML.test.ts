397a4fcdb350795532d2b2819b5d78a88694536f
nikita <nikita@crimsongold.ru>
2026-05-11 23:09:40 +0500

    test: :white_check_mark: покрыть экспорт перечисления

---
+++ packages/core/metadata/appliedObjects/metadataEnumeration/toXML.test.ts
@@ -0,0 +1,20 @@
+import { describe, expect, it } from "vitest"
+import { testExportAppliedObjectToXML } from "~/tests/appliedObject"
+import { full } from "./__fixtures__/full"
+import { minimal } from "./__fixtures__/minimal"
+import { MetadataEnumerationRules } from "./rules"
+
+describe("export MetadataEnumeration to XML", () => {
+  it.each([
+    { fixture: "full.xml", data: full },
+    { fixture: "minimal.xml", data: minimal },
+  ])("should export $fixture fixture", ({ fixture, data }) => {
+    const { result, expected } = testExportAppliedObjectToXML({
+      rule: MetadataEnumerationRules,
+      importMetaUrl: import.meta.url,
+      fixture,
+      data,
+    })
+    expect(result).toEqual(expected)
+  })
+})
