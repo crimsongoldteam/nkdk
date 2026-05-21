dd361833860c8ead6334a27767e715f59780e389
nikita <nikita@crimsongold.ru>
2026-05-21 15:33:23 +0500

    chore: :wrench: убрать упоминание yamlPartialOthers

--- packages/core/metadata/forms/elements/button/__fixtures__/data.ts
+++ packages/core/metadata/forms/elements/button/__fixtures__/data.ts
@@ -81,7 +81,7 @@ const commonButtonPartialYAML = {
   ЦветРамки: "Кирпичный" as const,
   Шрифт: { Вид: "КрупныйШрифтТекста" } as const,
   Картинка: "Печать" as const,
-  // Заголовок absent: yamlPartialOthers: true, only ru language present
+  // Заголовок absent: only ru language present
   ОтображениеПодсказки: "Всплывающая" as const,
   ОтображениеВКонтекстномМеню: "Нет" as const,
   Фигура: "Обычная" as const,
