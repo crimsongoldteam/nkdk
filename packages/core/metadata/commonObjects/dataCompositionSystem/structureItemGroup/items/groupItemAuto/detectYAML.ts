23a0f185af943e70790edb6e5bf44bc521d65d92
nikita <nikita@crimsongold.ru>
2026-05-22 22:39:58 +0500

    fix: :bug: импортировать объектные GroupItemField из группировок

--- packages/core/metadata/commonObjects/dataCompositionSystem/structureItemGroup/items/groupItemAuto/detectYAML.ts
+++ packages/core/metadata/commonObjects/dataCompositionSystem/structureItemGroup/items/groupItemAuto/detectYAML.ts
@@ -1,3 +1,4 @@
-export const detectGroupItemAutoYAML = (yaml: string) => {
+export const detectGroupItemAutoYAML = (yaml: unknown) => {
+  if (typeof yaml !== "string") return false
   return yaml.includes("[Авто]") || yaml.includes("([Авто])")
 }
