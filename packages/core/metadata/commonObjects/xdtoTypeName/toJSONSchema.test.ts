08b4ecfcadcb5c6a8cf031c666a64370cf23ad7b
nikita <nikita@crimsongold.ru>
2026-06-05 17:34:54 +0500

    test: :white_check_mark: поправить тест схемы XDTOTypeName

--- packages/core/metadata/commonObjects/xdtoTypeName/toJSONSchema.test.ts
+++ packages/core/metadata/commonObjects/xdtoTypeName/toJSONSchema.test.ts
@@ -1,9 +1,10 @@
 import { describe, expect, it } from "vitest"
+import { mockContext, mockRule } from "~/tests/mockContext"
 import { exportXDTOTypeNameToJSONSchema } from "./toJSONSchema"
 
 describe("export XDTOTypeName JSON schema", () => {
   it("exports expanded name object schema", () => {
-    expect(exportXDTOTypeNameToJSONSchema()).toMatchObject({
+    expect(exportXDTOTypeNameToJSONSchema({ context: mockContext, rule: mockRule, value: undefined })).toMatchObject({
       type: "object",
       required: ["ПространствоИмен", "Имя"],
       properties: {
