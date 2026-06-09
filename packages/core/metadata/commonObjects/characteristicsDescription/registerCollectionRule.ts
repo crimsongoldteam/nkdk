58a4150b16accdb5f62a830b801b696bc69d81f2
NoMoreVersionIn1C <andrew@crimsongold.ru>
2026-06-09 18:39:09 +0500

    fix: :bug: не добавлять XML-default характеристик

--- packages/core/metadata/commonObjects/characteristicsDescription/registerCollectionRule.ts
+++ packages/core/metadata/commonObjects/characteristicsDescription/registerCollectionRule.ts
@@ -5,6 +5,7 @@ import { importMetadataItemCollectionFromXML } from "~/metadata/orchestration/me
 import { importMetadataItemCollectionFromYAMLAsArray } from "~/metadata/orchestration/metadataCollection/fromYAML"
 import { exportMetadataCollectionToXML } from "~/metadata/orchestration/metadataCollection/toXML"
 import { exportMetadataCollectionToYAMLAsArray } from "~/metadata/orchestration/metadataCollection/toYAML"
+import type { ExportToXMLFunctionNew } from "~/metadata/orchestration/property/fn"
 import { CharacteristicsDescriptionRules } from "./rules"
 import { CharacteristicsDescriptions } from "./types"
 
@@ -15,18 +16,22 @@ export const importCharacteristicsDescriptionsFromXML = importMetadataItemCollec
 
 registerTypeRule("CharacteristicsDescriptions", "importFromXML", importCharacteristicsDescriptionsFromXML)
 
-registerTypeRule(
-  "CharacteristicsDescriptions",
-  "exportToXML",
-  (p: { context: ConfigurationContextWithExportToXML; rule: PropertyRule; value: CharacteristicsDescriptions }) =>
-    exportMetadataCollectionToXML({
-      context: p.context,
-      rule: p.rule,
-      data: p.value,
-      itemRule: CharacteristicsDescriptionRules,
-      xmlElement: "xr:Characteristic",
-    })
-)
+const exportCharacteristicsDescriptionsToXML: ExportToXMLFunctionNew = (p: {
+  context: ConfigurationContextWithExportToXML
+  rule: PropertyRule
+  value: CharacteristicsDescriptions
+  referenceMetadata?: CharacteristicsDescriptions
+}) =>
+  exportMetadataCollectionToXML({
+    context: p.context,
+    rule: p.rule,
+    data: p.value,
+    referenceData: p.referenceMetadata,
+    itemRule: CharacteristicsDescriptionRules,
+    xmlElement: "xr:Characteristic",
+  })
+
+registerTypeRule("CharacteristicsDescriptions", "exportToXML", exportCharacteristicsDescriptionsToXML)
 
 registerTypeRule("CharacteristicsDescriptions", "importFromYAML", (context, _rule, value) =>
   importMetadataItemCollectionFromYAMLAsArray({ context, itemRule: CharacteristicsDescriptionRules, yaml: value })
