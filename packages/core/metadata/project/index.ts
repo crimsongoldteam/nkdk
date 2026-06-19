82d61e4093655d22b957d50ee192bc8bb21fff7a
nikita <nikita@crimsongold.ru>
2026-06-19 08:45:15 +0500

    feat: :sparkles: экспортировать модель metadata-проекта

--- packages/core/metadata/project/index.ts
+++ packages/core/metadata/project/index.ts
@@ -1,10 +1,13 @@
 export {
   describeMetadataRuleResources,
   type MetadataProjectAssetDescriptor,
+  type MetadataProjectConfigurationYamlDescriptor,
   type MetadataProjectDynamicDescriptor,
   type MetadataProjectExternalXmlBaseDescriptor,
   type MetadataProjectExternalXmlDescriptor,
   type MetadataProjectExternalXmlPathDescriptor,
+  type MetadataProjectObjectXmlDescriptor,
+  type MetadataProjectPropertiesYamlDescriptor,
   type MetadataProjectResourceDescriptor,
   type MetadataProjectXmlDescriptor,
   type MetadataProjectYamlDescriptor,
