5d9ee58f9fef0f49cd0a0c4f0c6b8233d3e7572f
nikita <nikita@crimsongold.ru>
2026-08-15 09:58:30 +0500

    test: ⚡ стабилизировать lifecycle XML-import
    Закрывать независимые worker-пулы и удалять временные каталоги параллельно. Это оставляет запас до обязательного лимита файла при полном прогоне.

--- packages/rules/metadata/importFromXml/importConfigurationExtension.integration.test.ts
+++ packages/rules/metadata/importFromXml/importConfigurationExtension.integration.test.ts
@@ -39,11 +39,12 @@ const projectState = createImportProjectStateTestService({
 let importedExtension: Awaited<ReturnType<typeof importExtension>>
 
 afterAll(async () => {
-  await xmlImportWorkerPoolHandle.close()
-  await projectState.close()
-  for (const directory of temporaryDirectories.splice(0)) {
-    fs.rmSync(directory, { recursive: true, force: true })
-  }
+  await Promise.all([
+    xmlImportWorkerPoolHandle.close(),
+    projectState.close(),
+    ...temporaryDirectories.splice(0).map((directory) =>
+      fs.promises.rm(directory, { recursive: true, force: true })),
+  ])
 })
 
 describe("configuration extension XML import", () => {
